11734ddc158467a05252235aeb80242d36a9f6b4
Chifeng Wen <daviesx66@gmail.com>
2016-04-25 12:42:41 -0700

    fixed the bug happens when there are same node names appear in both networks; improved performance and reduced memory consumption.

--- alignmentDrawer/alignmentdrawerplugin/src/research/NodeSignatureManager.java
+++ alignmentDrawer/alignmentdrawerplugin/src/research/NodeSignatureManager.java
@@ -18,35 +18,67 @@
 package research;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import org.apache.commons.collections4.SetUtils;
-import org.apache.commons.collections4.bidimap.DualLinkedHashBidiMap;
 
 /**
  * Manage signature identification.
  *
  * @author davis
  */
-public class NodeSignatureManager {
+public class NodeSignatureManager implements Comparable {
+        
+        private final String COMMA = "-";
+        private final String SEPARATOR = ":";
+        
+       public class NodeSignature {
+               protected final String namespace;
+               protected final String id;
+               private final int m_hash;
 
-//        private final LinkedHashSet<String> m_namespaces = new LinkedHashSet<>();
-//        private final LinkedHashSet<String> m_ids = new LinkedHashSet<>();
-        private final DualLinkedHashBidiMap<String, String> m_node_id = new DualLinkedHashBidiMap<>();
+               public NodeSignature(String namespace, String id) {
+                       this.namespace = namespace;
+                       this.id = id;
+                       m_hash = namespace.hashCode() + id.hashCode();
+               }
+               
+               @Override
+               public boolean equals(Object o) {
+                       NodeSignature other = (NodeSignature) o;
+                       return id.equals(other.id) && namespace.equals(other.namespace);
+               }
 
-        private final String COMMA = "#";
-        private final String SEPARATOR = "/";
+                @Override
+                public int hashCode() {
+                        return m_hash;
+                }
+                
+               @Override
+                public String toString() {
+                        return namespace + SEPARATOR + id;
+                }
+       }
+       
+       private final ArrayList<NodeSignature> m_sigs = new ArrayList<>();
+        
+       private void __add_signature(String namespace, String id) {
+                for (NodeSignature sig : m_sigs) {
+                        if (id.hashCode() + namespace.hashCode() == sig.hashCode() &&
+                            sig.id.equals(id) && sig.namespace.equals(namespace)) {
+                                return;
+                        }
+                }
+                m_sigs.add(new NodeSignature(namespace, id));
+        }
+        
+        private void __clean_signatures() {
+                m_sigs.clear();
+        }
 
-        private void arrange_id_namespace(String sig_string) {
-                String[] parts = sig_string.split(SEPARATOR);
-                if (parts.length != 2)
-                        return;
-                String[] namespaces = parts[0].split(COMMA);
-                String[] ids = parts[1].split(COMMA);
-                for (int i = 0; i < ids.length; i ++) {
-                        if (namespaces[i].equals("") || ids[i].equals(""))
-                                continue;
-                        m_node_id.put(namespaces[i], ids[i]);
+        private void __arrange_id_namespace(String sig_string) {
+                String[] parts = sig_string.split(COMMA);
+                for (String part : parts) {
+                        String[] nsid = part.split(SEPARATOR);
+                        if (nsid.length == 2)
+                                __add_signature(nsid[0], nsid[1]);
                 }
         }
 
@@ -63,72 +95,69 @@ public class NodeSignatureManager {
 //        }
         
         public void override_with(String sig_string) {
-                clear();
-                arrange_id_namespace(sig_string);
+                __clean_signatures();
+                __arrange_id_namespace(sig_string);
         }
         
         public void append_with(String sig_string) {
-                arrange_id_namespace(sig_string);
+                __arrange_id_namespace(sig_string);
         }
         
         public void clear() {
-                m_node_id.clear();
+                __clean_signatures();
         }
         
         public void add_namespaced_id(String namespace, String id) {
-                m_node_id.put(namespace, id);
+                __add_signature(namespace, id);
         }
         
-        public Set<String> get_all_ids() {
-                return m_node_id.values();
-        }
-        
-        public Set<String> get_all_namespaces() {
-                return m_node_id.keySet();
+        public ArrayList<NodeSignature> get_all_node_signatures() {
+                return m_sigs;
         }
 
         @Override
         public boolean equals(Object o) {
-                if (!(o instanceof NodeSignatureManager)) {
-                        return false;
-                }
                 NodeSignatureManager other = (NodeSignatureManager) o;
-                Set<String> common_ids = SetUtils.intersection(get_all_ids(), other.get_all_ids());
-                for (String common_id : common_ids) {
-                        if (m_node_id.getKey(common_id).equals(other.m_node_id.getKey(common_id))) {
-                                return true;
+                for (NodeSignature sig0 : m_sigs) {
+                        for (NodeSignature sig1 : other.m_sigs) {
+                                if (sig0.hashCode() == sig1.hashCode() && sig0.equals(sig1))
+                                        return true;
                         }
                 }
                 return false;
         }
+        
+        @Override
+        public int compareTo(Object o) {
+                return equals(o) ? 0 : 1;
+        }
 
         @Override
         public int hashCode() {
-//                int hash = 7;
-//                hash = 53 * hash + Objects.hashCode(this.m_namespaces);
-//                hash = 53 * hash + Objects.hashCode(this.m_ids);
                 return 0;
         }
 
         @Override
         public String toString() {
+                if (m_sigs.isEmpty()) return "";
                 StringBuilder s = new StringBuilder();
-                List<String> keys = new ArrayList<>();
-                for (String ns : m_node_id.keySet()) {
-                        keys.add(ns);
-                        s.append(ns).append(COMMA);
-                }
-                s.append(SEPARATOR);
-                for (String namespace : keys) {
-                        s.append(m_node_id.get(namespace)).append(COMMA);
+                NodeSignature first = m_sigs.get(0);
+                s.append(first.namespace).append(SEPARATOR).append(first.id);
+                for (int i = 1; i < m_sigs.size(); i ++) {
+                        NodeSignature sig = m_sigs.get(i);
+                        s.append(COMMA).append(sig.namespace).append(SEPARATOR).append(sig.id);
                 }
                 return s.toString();
         }
 
         public String toSimplifiedString() {
+                if (m_sigs.isEmpty()) return "";
                 StringBuilder s = new StringBuilder();
-                for (String id : m_node_id.values()) {
-                        s.append(id).append(COMMA);
+                NodeSignature first = m_sigs.get(0);
+                s.append(first.id);
+                for (int i = 1; i < m_sigs.size(); i ++) {
+                        NodeSignature sig = m_sigs.get(i);
+                        s.append(COMMA).append(sig.id);
                 }
                 return s.toString();
         }
