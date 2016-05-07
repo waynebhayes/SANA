674377e23a17182248ab9f1476f291190cc55ce2
Wayne Hayes <whayes@uci.edu>
2016-05-06 18:21:13 -0700

    Added default params.
    Former-commit-id: 1a1e1e9b4fc4d10fae10be54ddf1715b8d42c242

--- src/methods/wrappers/OptNetAlignWrapper.cpp
+++ src/methods/wrappers/OptNetAlignWrapper.cpp
@@ -7,13 +7,14 @@ using namespace std;
 
 const string CONVERTER = "python converter.py";
 const string PROGRAM   = "./optnetalign";
+const string GLOBAL_PARAMETERS = "--total --cxrate 0.05 --cxswappb 0.75 --mutrate 0.05 --mutswappb 0.0001 --oneobjrate 0.75 --dynparams --generations 1000000000 --hillclimbiters 10000 --finalstats";
 
 OptNetAlignWrapper::OptNetAlignWrapper(Graph* G1, Graph* G2, string args): WrappedMethod(G1, G2, "OPTNETALIGN", args) {
 	wrappedDir = "wrappedAlgorithms/OptNetAlign";
 }
 
 void OptNetAlignWrapper::loadDefaultParameters() {
-	parameters = "--total --s3 --nthreads 24 --cxrate 0.05 --cxswappb 0.75 --mutrate 0.05 --mutswappb 0.0001 --oneobjrate 0.75 --dynparams --popsize 2000 --generations 1000000000 --hillclimbiters 10000 --timelimit 360 --finalstats";
+	parameters = "--s3 --nthreads 1 --popsize 2000 --timelimit 5";
 }
 
 string OptNetAlignWrapper::convertAndSaveGraph(Graph* graph, string name) {
@@ -27,7 +28,7 @@ string OptNetAlignWrapper::generateAlignment() {
 	//exec("cd " + wrappedDir + "/src; make optnetalignubuntu; mv optnetalign ../");
 	//exec("cd " + wrappedDir + "; chmod +x " + PROGRAM);
 	execPrintOutput("cd " + wrappedDir + "; " + PROGRAM + " --net1 " +
-			g1File + " --net2 " + g2File + " " + parameters + " --outprefix " + outputname);
+			g1File + " --net2 " + g2File + " " + GLOBAL_PARAMETERS + " " + parameters + " --outprefix " + outputname);
 
 	outputname += "_0.aln";
 
