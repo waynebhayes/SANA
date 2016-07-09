fb08911c5242d309b0e8e1d4060c24b0abb7f36e
Wayne Hayes <whayes@uci.edu>
2016-07-09 11:15:10 -0700

    removed paper.
    Former-commit-id: aa3f368aa0cd5340f9335226b4c69ddc7fec33a5

--- compare.sh
+++ compare.sh
@@ -3,10 +3,17 @@ die() { echo "FATAL ERROR: $@" >&2; exit 1
 }
 
 SEQ=false
-USAGE="$0 [-seq] otherAligner g1 g2"
-case "$1" in
--seq) SEQ=true; shift;;
-esac
+FORCE=false
+
+USAGE="$0 [-seq] [-f] otherAligner g1 g2"
+while :; do
+    case "$1" in
+    -seq) SEQ=true; shift;;
+    -f) FORCE=true; shift;;
+    -*) die "unknown option '$1'";;
+    *) break ;;
+    esac
+done
 M="$1"
 G1="$2"
 G2="$3"
@@ -51,8 +58,9 @@ elif echo $M | grep -q -i '^netal$'; then
     Margs=
     Sargs=' -s3 0.4 -ec 0.6 '
 elif echo $M | grep -q -i '^gedevo$'; then
-    Margs=" -wrappedArgs \"--maxsecs 3600 --threads 4 \""
-    Sargs=' -s3 0 -ec 1 '
+    Tcmp=m/9; Tmin=10; Tmax=30
+    Margs=" -wrappedArgs \"--maxsecs 36000 --threads 8 \""
+    Sargs=' -s3 0.35 -ec 0.65 '
     Mseq=" -wrappedArgs \"--maxsecs 3600 --threads 4 --blastpairlist sequence/bitscores/${G1}-${G2}.bitscores\""
     Sseq=" -ec .5 -s3 0.5 -sequence 0.5 "
 elif echo $M | grep -q -i '^wave$'; then
@@ -61,7 +69,9 @@ elif echo $M | grep -q -i '^wave$'; then
 elif echo $M | grep -q -i '^spinal$'; then
     Margs=""
     Sargs=''
-    Tcmp=m/10; Tmin=5; Tmax=30
+    Mseq=" -sequence 1 -ec 1 -alpha 0.5 -wrappedArgs \"-I -ns sequence/SPINAL/${G1}-${G2}\""
+    Sseq=" -ec 1 -sequence 300 "
+    Tcmp=m; Tmin=3; Tmax=20
 elif echo $M | grep -q -i '^piswap$'; then
     Margs=" -wrappedArgs sims/graphlet/$G1-$G2.sim "
     Sargs=''
@@ -79,15 +89,21 @@ fi
 set -x
 if $SEQ; then
     DIR=Mseq
-    eval ./sana.$M -method $M $Mseq  -g1 $G1 -g2 $G2 -o $DIR/$M/$G1-$G2 >$DIR/$M/$G1-$G2.stdout 2>$DIR/$M/$G1-$G2.stderr
+    if $FORCE || [ ! -f $DIR/$M/$G1-$G2.out ]; then
+	eval ./sana.$M -method $M $Mseq  -g1 $G1 -g2 $G2 -o $DIR/$M/$G1-$G2 >$DIR/$M/$G1-$G2.stdout 2>$DIR/$M/$G1-$G2.stderr
+    fi
 else
     DIR=M
-    eval ./sana.$M -method $M "$Margs" -g1 $G1 -g2 $G2 -o $DIR/$M/$G1-$G2 >$DIR/$M/$G1-$G2.stdout 2>$DIR/$M/$G1-$G2.stderr
+    if $FORCE || [ ! -f $DIR/$M/$G1-$G2.out ]; then
+	eval ./sana.$M -method $M "$Margs" -g1 $G1 -g2 $G2 -o $DIR/$M/$G1-$G2 >$DIR/$M/$G1-$G2.stdout 2>$DIR/$M/$G1-$G2.stderr
+    fi
 fi
 T="MAX($Tmin,MIN($Tcmp,$Tmax))"
 T=`grep -i 'execution time' $DIR/$M/$G1-$G2.out | wawk '{m=$NF/60; print '"$T"'}'`
-if $SEQ; then
-    eval ./sana.$M -t $T -tinitial auto -tdecay auto $Sseq  -g1 $G1 -g2 $G2 -o $DIR/$M/sana.$G1-$G2 >$DIR/$M/sana.$G1-$G2.stdout 2>$DIR/$M/sana.$G1-$G2.stderr
-else
-    ./sana.$M -t $T -tinitial auto -tdecay auto $Sargs -g1 $G1 -g2 $G2 -o $DIR/$M/sana.$G1-$G2 >$DIR/$M/sana.$G1-$G2.stdout 2>$DIR/$M/sana.$G1-$G2.stderr
+if $FORCE || [ ! -f $DIR/$M/sana.$G1-$G2.out ]; then
+    if $SEQ; then
+	eval ./sana.$M -t $T -tinitial auto -tdecay auto $Sseq  -g1 $G1 -g2 $G2 -o $DIR/$M/sana.$G1-$G2 >$DIR/$M/sana.$G1-$G2.stdout 2>$DIR/$M/sana.$G1-$G2.stderr
+    else
+	./sana.$M -t $T -tinitial auto -tdecay auto $Sargs -g1 $G1 -g2 $G2 -o $DIR/$M/sana.$G1-$G2 >$DIR/$M/sana.$G1-$G2.stdout 2>$DIR/$M/sana.$G1-$G2.stderr
+    fi
 fi
