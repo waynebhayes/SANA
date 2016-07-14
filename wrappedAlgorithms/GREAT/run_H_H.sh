ebe77ab8879a27eb40ff6c31d8321810515033fa
Wayne Hayes <whayes@uci.edu>
2016-07-14 07:50:59 -0700

    Replaced "read" statements with command line args in GREAT's run*.sh
    Former-commit-id: 342ebf7392f63b368378f75f09ebf917314ef3af

--- wrappedAlgorithms/GREAT/run_H_H.sh
+++ wrappedAlgorithms/GREAT/run_H_H.sh
@@ -1,14 +1,9 @@
+networkA="$1"
+networkB="$2"
+shift 2
+# alphas are on remainder of command line.
 
-#gets both networks and alpha parameter from user
-echo "Usage: networkA networkB alpha_parameter"
-echo " "
-echo "The networks must have extension .gw, but do not place that in the parameter"
-
-read networkA 
-read networkB
-
-read -a arr
-for i in "${arr[@]}"
+for i in "$@"
 do
     alpha=$i
 
