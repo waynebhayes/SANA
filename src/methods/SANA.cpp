fbe48a00852decc4f6f7e42c829bd5c301bee61e
Hudson Hughes <96hudson@gmail.com>
2016-09-21 11:51:45 -0700

    fixed tabbing
    Former-commit-id: e1d54334cd1fd47e19f48b6930062968045df372

--- src/methods/SANA.cpp
+++ src/methods/SANA.cpp
@@ -972,31 +972,31 @@ double SANA::scoreForTInitial(double TInitial) {
 
 double SANA::findTInitialByLinearRegression(){
 	//Look for a cache file matching the measure and the graphs and fill the cache variable with the file
-    map<double, double> cache;
-    std::ifstream cacheFile(mkdir("scores") + "scores_" + G1->getName() + "_" + G2->getName() + ".txt");
-    double a, b;
-    cout << "Finding optimal initial temperature using linear regression fit of scores between temperature extremes\n";
-    //set the float precisison of the stream. This is needed whenever a file is written
-    cout << "Retrieving 50 Samples" << endl;
-    int progress = 0;
-    cout.precision(17);
-    cout << fixed;
-    while (cacheFile >> a >> b){
+	map<double, double> cache;
+	std::ifstream cacheFile(mkdir("scores") + "scores_" + G1->getName() + "_" + G2->getName() + ".txt");
+	double a, b;
+	cout << "Finding optimal initial temperature using linear regression fit of scores between temperature extremes\n";
+	//set the float precisison of the stream. This is needed whenever a file is written
+	cout << "Retrieving 50 Samples" << endl;
+	int progress = 0;
+	cout.precision(17);
+	cout << fixed;
+	while (cacheFile >> a >> b){
 		cache[a] = b;
-    }
-    cacheFile.close();
-    //Make a file out stream to send scores to the cache file if the temperatures score isn't already there
-    ofstream cacheOutStream(mkdir("scores") + "scores_" + G1->getName() + "_" + G2->getName() + ".txt", std::ofstream::out | std::ofstream::app);
-    cacheOutStream.precision(17);
-    cacheOutStream << fixed;
-    //Map that pairs temperatures (in log space) to scores, then we add the pairs already in the cache
-    map<double, double> scoreMap;
-    //start first instance of linear regression by filling the map with 20 pairs over 10E-10 to 10E!0
-    for(double i = -10.0; i < 10.0; i = i + 1.0){
+	}
+	cacheFile.close();
+	//Make a file out stream to send scores to the cache file if the temperatures score isn't already there
+	ofstream cacheOutStream(mkdir("scores") + "scores_" + G1->getName() + "_" + G2->getName() + ".txt", std::ofstream::out | std::ofstream::app);
+	cacheOutStream.precision(17);
+	cacheOutStream << fixed;
+	//Map that pairs temperatures (in log space) to scores, then we add the pairs already in the cache
+	map<double, double> scoreMap;
+	//start first instance of linear regression by filling the map with 20 pairs over 10E-10 to 10E!0
+	for(double i = -10.0; i < 10.0; i = i + 1.0){
 		if(cache.find(i) == cache.end()){
 			double score = scoreForTInitial(pow(10, i));
-		    cacheOutStream << i << " " << score << endl;
-		    scoreMap[i] = score;
+			cacheOutStream << i << " " << score << endl;
+			scoreMap[i] = score;
 		}else{
 			scoreMap[i] = cache[i];
 		}
@@ -1005,52 +1005,52 @@ double SANA::findTInitialByLinearRegression(){
 	}
 	//actually perform the linear regression
 	LinearRegression linearRegression;
-    linearRegression.setup(scoreMap);
-    //The "run" linear regression function returns a tuple
-    tuple<int, double, double, int, double, double, double, double> regressionResult = linearRegression.run();
-    //pull the temperature bounds from the tuple and strech them a bit
-    double lowerEnd = get<1>(regressionResult) - 1.0;
-    double upperEnd = get<4>(regressionResult) + 1.0;
-    //fill the score map with 30 more pairs betweem the boundaries
-    for(double i = lowerEnd; i < upperEnd; i += (upperEnd - lowerEnd) / 30.0){
-    	if(cache.find(i) == cache.end()){
+	linearRegression.setup(scoreMap);
+	//The "run" linear regression function returns a tuple
+	tuple<int, double, double, int, double, double, double, double> regressionResult = linearRegression.run();
+	//pull the temperature bounds from the tuple and strech them a bit
+	double lowerEnd = get<1>(regressionResult) - 1.0;
+	double upperEnd = get<4>(regressionResult) + 1.0;
+	//fill the score map with 30 more pairs betweem the boundaries
+	for(double i = lowerEnd; i < upperEnd; i += (upperEnd - lowerEnd) / 30.0){
+		if(cache.find(i) == cache.end()){
 			double score = scoreForTInitial(pow(10, i));
 		    cacheOutStream << i << " " << score << endl;
-		    scoreMap[i] = score;
+			scoreMap[i] = score;
 		}else{
 			scoreMap[i] = cache[i];
 		}
 		progress++;
 		cout << "\r" << progress << " of 50" << std::flush;
-    }
-    cout << endl;
-    //close the cahce file stream
-    cacheOutStream.close();
-    //run another linear regression instance
-    LinearRegression linearRegression2;
-    linearRegression2.setup(scoreMap);
-    regressionResult = linearRegression2.run();
-    cout << "lower index: " << get<0>(regressionResult) << " ";
-    cout << "upper index: " << get<3>(regressionResult) << endl;
-    cout << "lower temperature: " << pow(10, get<2>(regressionResult)) << " ";
-    cout << "higher temperature: " << pow(10, get<5>(regressionResult)) << endl;
-    cout << "line 1 height: " << get<6>(regressionResult) << " ";
-    cout << "line 3 height: " << get<7>(regressionResult) << endl;
-    double currentTemperature = get<5>(regressionResult);
-    cout << "Current Temperature " << pow(10, get<5>(regressionResult));
-    double currentPBad = pForTInitial(pow(10, get<5>(regressionResult)));
-    cout << " Current PBad " << currentPBad << endl;
-    //ncreasing temperature until an acceptable probability is reached
-    int iteration = 1;
-    cout << "Increasing temperature until an acceptable probability is reached. " << endl;
-    while(currentPBad < 0.985){
-    	currentTemperature += 0.4;
-    	currentPBad = pForTInitial(pow(10, currentTemperature));
-    	cout << iteration << ": Temperature: " << pow(10, currentTemperature) << " PBad: " << currentPBad << endl;
-    	iteration++;
-    }
-    cout << "Final Temperature: " <<  pow(10, currentTemperature) << " Final P(Bad): " << currentPBad << endl;
-    return pow(10, currentTemperature);
+	}
+	cout << endl;
+	//close the cahce file stream
+	cacheOutStream.close();
+	//run another linear regression instance
+	LinearRegression linearRegression2;
+	linearRegression2.setup(scoreMap);
+	regressionResult = linearRegression2.run();
+	cout << "lower index: " << get<0>(regressionResult) << " ";
+	cout << "upper index: " << get<3>(regressionResult) << endl;
+	cout << "lower temperature: " << pow(10, get<2>(regressionResult)) << " ";
+	cout << "higher temperature: " << pow(10, get<5>(regressionResult)) << endl;
+	cout << "line 1 height: " << get<6>(regressionResult) << " ";
+	cout << "line 3 height: " << get<7>(regressionResult) << endl;
+	double currentTemperature = get<5>(regressionResult);
+	cout << "Current Temperature " << pow(10, get<5>(regressionResult));
+	double currentPBad = pForTInitial(pow(10, get<5>(regressionResult)));
+	cout << " Current PBad " << currentPBad << endl;
+	//ncreasing temperature until an acceptable probability is reached
+	int iteration = 1;
+	cout << "Increasing temperature until an acceptable probability is reached. " << endl;
+	while(currentPBad < 0.985){
+		currentTemperature += 0.4;
+		currentPBad = pForTInitial(pow(10, currentTemperature));
+		cout << iteration << ": Temperature: " << pow(10, currentTemperature) << " PBad: " << currentPBad << endl;
+		iteration++;
+	}
+	cout << "Final Temperature: " <<  pow(10, currentTemperature) << " Final P(Bad): " << currentPBad << endl;
+	return pow(10, currentTemperature);
 }
 
 string SANA::getFolder(){
