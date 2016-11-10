63128f14b43666ab169442b0d5950651d951fcac
neehan <nzxcvb5@gmail.com>
2016-11-10 20:33:04 +0600

    added the code to generate Orbit Matrix
    Former-commit-id: 0646199db95db2f73fe13824072cc118667f8319

---
+++ test/Graphette/src/init/GenerateOrbitMatrix.cpp
@@ -0,0 +1,36 @@
+#include "../Graphette.hpp"
+#include <cstdlib>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void generateOrbitMatrix(uint n){
+	string inname = "data/canon_list"+to_string(n)+".txt";
+	string outname = "data/orbit_map"+to_string(n)+".txt";
+	ifstream fin(inname);
+	ofstream fout(outname);
+	uint num, cgraph, orbitId = 0;
+	vector<uint> canonical;
+
+	fin >> num;
+	fout << "#ORBIT ID FOR EACH NODE OF EACH " << n << "-CANONICAL GRAPHETTE IN ASCENDING ORDER\n";
+	while(fin >> cgraph){
+		Graphette g = Graphette(n, cgraph);
+		vector<uint> idList(n);		
+		vector<vector<ushort>> orbits = g.getOrbits();
+		for(auto orbit : orbits){
+			for(auto node : orbit){
+				idList[node] = orbitId;
+			}
+			orbitId++;
+		}
+		for(uint i=0; i < idList.size(); i++){
+				fout << idList[i] << " ";
+			}
+		fout << endl;
+	}
+}
