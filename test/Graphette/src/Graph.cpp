2ba0726ea47b3b7550e0c06c659c949fec37e968
neehan <nzxcvb5@gmail.com>
2016-11-13 23:38:14 +0600

    add getSampleGraphette() getNeighbors() & explore()
    Former-commit-id: eb99573a596131a7904dde43f861c93a84f4ced5

--- test/Graphette/src/Graph.cpp
+++ test/Graphette/src/Graph.cpp
@@ -88,6 +88,10 @@ void Graph::removeEdge(uint node1, uint node2){
 	}
 }
 
+bool Graph::getEdge(uint node1, uint node2){
+	return adjMatrix_(node1, node2);
+}
+
 uint Graph::getNumNodes(){
 	return numNodes_;
 }
@@ -105,15 +109,39 @@ void Graph::printAdjMatrix()
 	adjMatrix_.print();
 }
 
-Graph* Graph::getSubGraph(vector<uint>& nodes){
-	uint n = nodes.size();
-	vector<bool> bits;
-	for(uint i = 0; i < n; i++){
-		for(uint j = i+1; j < n; j++){
-			bits.push_back(adjMatrix_(nodes[i], nodes[j]));
+Graphette* Graph::getSampleGraphette(uint numNodes, uint radius){
+	if(numNodes == 0){
+		throw out_of_range("Graph::getSampleGraphette(): numNodes can't be 0");
+	}
+	else{
+		//Select a random node
+		uint node = xrand(0, numNodes);
+		vector<uint> nbors = this->getNeighbors(node, radius);
+		Graphette* g = new Graphette();
+		return g;
+	}
+}
+
+vector<uint> Graph::getNeighbors(uint node, uint radius){
+	vector<uint> nbors;
+	auto nodes = this->explore(node, radius);
+	nbors.assign(nodes.begin(), nodes.end());
+	return nbors;
+}
+
+set<uint> Graph::explore(uint seed, uint radius){
+	set<uint> nbors;
+	nbors.insert(seed);
+	for(uint i=0; i<numNodes_; i++){
+		if(seed == i) continue;
+		if(adjMatrix_(seed, i)){
+			if(radius == 1) 
+				nbors.insert(i);
+			else if(radius > 1){
+				auto temp = this->explore(i, radius-1);
+				nbors.insert(temp.begin(), temp.end());
+			}
 		}
 	}
-	HalfMatrix m(n, bits);
-	Graph* g = new Graph(m);
-	return g;
+	return nbors;
 }
