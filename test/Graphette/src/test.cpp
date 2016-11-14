886476a1ba514d3f719ad02d0ba0a5e890efd9ab
neehan <nzxcvb5@gmail.com>
2016-11-14 23:26:48 +0600

    final frontend for querying a k-graphette (almost complete)
    Former-commit-id: 283a934c95cb052af38d7fad4065d9af4b3c66dc

--- test/Graphette/src/test.cpp
+++ test/Graphette/src/test.cpp
@@ -8,26 +8,29 @@
 #include <string>
 #include <set>
 #include <utility>
+#include <ctime>
 #include "utils/xrand.hpp"
 
 using namespace std;
 
 int main(int arg, char*argv[]){
-	uint m, n, seed = atoi(argv[1]), radius = atoi(argv[2]), len= atoi(argv[3]);
+	uint m, n, k = atoi(argv[1]), radius = atoi(argv[2]), limit= atoi(argv[3]);
 	vector<pair<uint, uint>> edgelist;
-	ifstream fin("test/edgelist.txt");
+	ifstream fin("test/edgelist.txt"), forbit("data/orbit_map"+to_string(k)+".txt");
+	string temp;
+	getline(forbit, temp);
 	while(fin >> m >> n){
 		edgelist.push_back(make_pair(m, n));
 	}
+	forbit >> m;
 	Graph g(edgelist);
-	auto nbors = g.neighbors(seed, radius);
-	for(auto nbor: nbors)
-		cout << nbor << " ";
-	cout << endl;
-	xshuffle(nbors, len);
-	//nbors.resize(len);
-	for(auto nbor: nbors)
-		cout << nbor << " ";
+	vector<vector<bool>> sigMatrix(g.numNodes(), vector<bool>(m, false));//each row for each node
+	
+	for(uint i=0; i<limit; i++){
+		Graphette* gh = g.sampleGraphette(k, radius);
+
+		delete gh;
+	}
 	cout << endl;
 	return 0;
 }
