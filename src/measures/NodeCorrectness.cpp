723793090d50f05469616c9f2ff137357a522653
Christian Ryan Godfirnon <cgodfirn@odin.ics.uci.edu>
2016-11-27 20:26:01 -0800

    Sanity Checks for NC truealignment
    Former-commit-id: fcd7d89ec3b79e56ebac185470d66055fda72ace

--- src/measures/NodeCorrectness.cpp
+++ src/measures/NodeCorrectness.cpp
@@ -24,11 +24,26 @@ vector<ushort> NodeCorrectness::convertAlign(const Graph& G1, const Graph& G2, c
     map<string,ushort> mapG1 = G1.getNodeNameToIndexMap();
     map<string,ushort> mapG2 = G2.getNodeNameToIndexMap();
     vector<ushort> alignment(G1.getNumNodes(), G2.getNumNodes());
+    bool g2Used[G2.getNumNodes()];
+    for(unsigned int i = 0; i < G2.getNumNodes(); ++i)
+	g2Used[i] = false;
     for(unsigned int i = 0; i < E.size()/2; ++i)
     {
         string n1 = E[2*i];
 	string n2 = E[2*i+1];
-	alignment[mapG1[n1]] = mapG2[n2];
+	unsigned int g1pos,g2pos;
+	try{
+	    g1pos = mapG1.at(n1);
+	    g2pos = mapG2.at(n2);
+	}catch(...){
+		throw std::runtime_error("Truealignment contains a node which does not exist");
+	}
+	if(alignment[g1pos] != G2.getNumNodes() || g2Used[g2pos])
+		throw std::runtime_error("Truealignment is not One to One");
+	else
+		g2Used[g2pos] = true;
+
+	alignment[mapG1.at(n1)] = mapG2.at(n2);
     }
     alignment.push_back(E.size()/2);
     return alignment;
