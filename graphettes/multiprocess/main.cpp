1945832d2fb9ba1e2cddac79091eb7e20aca43ed
pochienc <pochienc@uci.edu>
2016-12-18 23:30:57 -0800

    Working version of parallelized permutation mapping
    Former-commit-id: f3879108baf409bd4773acdf4ee1db0ade036f77

--- graphettes/multiprocess/main.cpp
+++ graphettes/multiprocess/main.cpp
@@ -21,31 +21,8 @@ int main(int arg, char* argv[])
 	int block_size = atoi(argv[3]);
 	int start = atoi(argv[4]);
 	int end = atoi(argv[5]);
-	std::vector<string> graph_canonical;
-	std::vector<std::vector<unsigned int>> mapping;
-	std::vector<string> permutations(block_size);
 	
-	graph_canonical = generate_canonical(num_nodes, mapping, permutations, block_size, start, end);
-
-	ofstream fcanon("canonical_decimal_representation"+to_string(num_nodes)+"_"+std::to_string(variation)+".txt"), 
-			fmap("canon_map"+to_string(num_nodes)+"_"+std::to_string(variation)+".txt"), 
-			fpermutation("permutation_map"+to_string(num_nodes)+"_"+std::to_string(variation)+".txt");
-	
-	// For writing to file
-	// This will show the decimal representations of adjMatrix of each Graph
-	std::cout << "Canonical Graph adjMatrix dec_rep: ";
-	for (string g : graph_canonical)
-		fcanon << g.substr(1,g.length()) << " ";
-
-	int canonical_node = 0;
-	for (std::vector<unsigned int> s: mapping) {
-		canonical_node = s[0];
-		for (unsigned int m: s) {
-			fmap << m << " ";
-			fpermutation << m << "\t" << canonical_node << "\t" << permutations[m - variation*block_size] << "\n";
-		}
-		fmap << "\n";
-	}     
+	generate_initial_canonical(num_nodes, variation, block_size, start, end);
 	
 	return 0;
 }
