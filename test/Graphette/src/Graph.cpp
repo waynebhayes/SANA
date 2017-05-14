e448b31e2d04f88071280f58d7a638e70690a91a
neehan <nzxcvb5@gmail.com>
2017-05-14 11:24:55 +0600

    added len > gone
    Former-commit-id: d863d8ade37dfee0bfb92210b74c2cbff44c477a

--- test/Graphette/src/Graph.cpp
+++ test/Graphette/src/Graph.cpp
@@ -130,11 +130,7 @@ Graphette* Graph::sampleGraphette(ullint k, ullint node1, ullint node2){
 		}
 		while(nodes.size() < k){
 			ullint i, node;
-<<<<<<< HEAD
-			if( (double) gone / len < 0.9){
-=======
-			if( (double) (len - gone) / len > 0){ // insist on graphlet before accepting a disconnect
->>>>>>> 3398caa5764519beef0f8c94f8792fee8ca03d13
+			if(len > gone){ // insist on graphlet before accepting a disconnect
 				do{ 
 					i = xrand(0, len);
 				}while(candidates[i] == -1);
