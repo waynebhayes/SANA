f5c2d35a04cdc0df247a5f14e803f171fb14c01f
Wayne Hayes <whayes@uci.edu>
2017-07-16 08:08:24 -0700

    "mkdir -p" on remote machine and edit jobs-done.txt stream before deciding remaining work
    Former-commit-id: 7414b622d418e0894fa2e404b061646abf4e7073

--- overseer.sh
+++ overseer.sh
@@ -69,7 +69,7 @@ else
     # Now get temperature schedule and SES denominator (the latter requires *.out files so don't use -scheduleOnly)
     mkdir -p $OUTDIR/dir-init
     /bin/rm -rf networks/$NAME-shadow0
-    ls "$@" | awk '{file=$0;gsub(".*/",""); gsub(".el$",""); gsub(".gw$",""); printf "'$SANA' -t 1 -s3 0 -ses 1 -fg1 %s -fg2 '$OUTDIR/dir0/$NAME-shadow0.el' -o '$OUTDIR'/dir-init/%s 2>'$OUTDIR'/dir-init/%s.stderr\n", file,$0,$0}' | parallel_delay networks/$NAME-shadow0/$NAME-shadow0.gw
+    ls "$@" | awk '{file=$0;gsub(".*/",""); gsub(".el$",""); gsub(".gw$",""); printf "mkdir -p '$OUTDIR/dir-init';'$SANA' -t 1 -s3 0 -ses 1 -fg1 %s -fg2 '$OUTDIR/dir0/$NAME-shadow0.el' -o '$OUTDIR'/dir-init/%s 2>'$OUTDIR'/dir-init/%s.stderr\n", file,$0,$0}' | parallel_delay networks/$NAME-shadow0/$NAME-shadow0.gw
     grep '^Initial Temperature: ' $OUTDIR/dir-init/*.stderr | sed -e "s,$OUTDIR/dir-init/,," -e 's/\.stderr//' -e 's/:/ /' | awk '{print $1,$4,$7}' > $OUTDIR/dir-init/tinitial-final.txt
     grep '^tdecay' $OUTDIR/dir-init/*.stderr | sed -e "s,$OUTDIR/dir-init/,," -e 's/.stderr//' -e 's/:/ /' | awk '{print $1,$3}' > $OUTDIR/dir-init/tdecay.txt
 
@@ -89,18 +89,18 @@ do
     echo ''
     echo -n ---- ITER $i ----- `date`
     i1=`expr $i + 1`
-    if [ -f $OUTDIR/dir$i1/$NAME-shadow$i1.el ]; then continue; fi
     mkdir -p $OUTDIR/dir$i1
+    if [ -f $OUTDIR/dir$i1/$NAME-shadow$i1.el ]; then continue; fi
     for g
     do
 	bg=`basename $g .gw`
 	bg=`basename $bg .el`
 	echo shadow-$bg.out.align >> $OUTDIR/dir$i1/expected-outFiles.txt
-	named-col-prog 'if(name!="'$bg'")next; i='$i';ITER='$ITER'; e0=log(tinitial);e1=log(tfinal);printf "'$SANA' -multi-iteration-only -s3 0 -ses 1 -t '$T_ITER' -fg1 '$g' -fg2 '$OUTDIR/dir$i/$NAME-shadow$i.el' -tinitial %g -tdecay %g -o '$OUTDIR/dir$i1/shadow-$bg' 2>'$OUTDIR/dir$i1/shadow-$bg.stderr' -startalignment '$OUTDIR/dir$i/shadow-$bg.out'\n", tinitial*exp((e1-e0)*i/ITER),tdecay/ITER' $OUTDIR/dir-init/schedule.tsv
+	named-col-prog 'if(name!="'$bg'")next; i='$i';ITER='$ITER'; e0=log(tinitial);e1=log(tfinal);printf "mkdir -p '$OUTDIR/dir$i1';'$SANA' -multi-iteration-only -s3 0 -ses 1 -t '$T_ITER' -fg1 '$g' -fg2 '$OUTDIR/dir$i/$NAME-shadow$i.el' -tinitial %g -tdecay %g -o '$OUTDIR/dir$i1/shadow-$bg' 2>'$OUTDIR/dir$i1/shadow-$bg.stderr' -startalignment '$OUTDIR/dir$i/shadow-$bg.out'\n", tinitial*exp((e1-e0)*i/ITER),tdecay/ITER' $OUTDIR/dir-init/schedule.tsv
     done | sort -u > $OUTDIR/dir$i/jobs.txt
     /bin/rm -rf networks/$NAME-shadow$i
     while [ `ls $OUTDIR/dir$i1 | fgrep -f $OUTDIR/dir$i1/expected-outFiles.txt | tee $OUTDIR/dir$i/jobs-done.txt | wc -l` -lt `echo "$@" | wc -w` ]; do
-	fgrep -v -f $OUTDIR/dir$i/jobs-done.txt $OUTDIR/dir$i/jobs.txt | parallel_delay networks/$NAME-shadow$i/$NAME-shadow$i.gw # wait until that file is created before parallel'ing
+	sed 's/\.out\.align/.stderr/' $OUTDIR/dir$i/jobs-done.txt | fgrep -v -f - $OUTDIR/dir$i/jobs.txt | parallel_delay networks/$NAME-shadow$i/$NAME-shadow$i.gw # wait until that file is created before parallel'ing
     done
     ./shadow2align.sh $OUTDIR/dir$i1/*.align > $DIR/dir$i1/multiAlign.tsv
     ./scripts/create_shadow.py -c -s $MAX_NODES -a $OUTDIR/dir$i1/shadow-*.out -n "$@" >$OUTDIR/dir$i1/$NAME-shadow$i1.el || die "$OUTDIR/dir$i1/$NAME-shadow$i1.el network creation failed"
