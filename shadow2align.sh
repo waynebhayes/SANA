777601d4dbb46127b66e711fc2acde3eae25d8d5
Wayne Hayes <whayes@uci.edu>
2017-09-24 13:01:20 -0700

    removed extraneous code
    Former-commit-id: 6c79e305b43de75a99e5bc7131e206af327699a9

--- shadow2align.sh
+++ shadow2align.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-USAGE="$0 [list of shadow-network alignment files in 2-column format]"
+USAGE="$0 [-V] [list of shadow-network alignment files in 2-column format]"
 
-hawk 'function Fatal(s){printf "FATAL ERROR: %s\n",s>"/dev/fd/2"; exit(1);}
+VERBOSE=0
+
+case "$1" in
+-V) VERBOSE=1; shift ;;
+esac
+
+hawk 'function Fatal(s){printf "FATAL ERROR: %s\n",s >"/dev/fd/2"; exit(1);}
     ARGIND>0{
-	if(ARGIND!=net){printf "reading file %s\n",FILENAME >"/dev/fd/2"; net=ARGIND;netName[net-1]=FILENAME}
+	if(ARGIND!=net){net=ARGIND;netName[net-1]=FILENAME;if('$VERBOSE')printf "reading file %s\n",FILENAME >"/dev/fd/2"}
 	if(index($1,"shadow")>0)
 	    {shadowNode=$1;mappedNode=$2}
 	else if(index($2,"shadow")>0)
@@ -26,36 +32,3 @@ hawk 'function Fatal(s){printf "FATAL ERROR: %s\n",s>"/dev/fd/2"; exit(1);}
 	    print lineOut
 	}
     }' "$@"
-
-exit
-
-
-	net++;
-	printf "%d total alignment files %d\n", net;
-	for(i=0;i<NC;i++)
-	    for(j=i+1;j<NC;j++)
-	    {
-		#printf "Cluster (%d,%d) = \n\t(%s,\n\t%s)\n", i,j,cluster[i],cluster[j];
-		E=s=0;
-		for(k=0;k<net;k++)
-		    if(A[i][k]!="_"&&A[j][k]!="_")
-		    {
-			s++;
-			if(edgeList[k][A[i][k]][A[j][k]])
-			{
-			    #printf "edge(%s,%s,%s) exists\n",netName[k],A[i][k],A[j][k]
-			    TE++;
-			    E++;
-			} else {
-			    #printf "edge(%s,%s,%s) NOT\n",netName[k],A[i][k],A[j][k]
-			}
-		    }
-		if(E>1)
-		{
-		    printf "Cluster (%d,%d) = \n\t(%s,\n\t%s)\nE=%d s=%d E*E/s=%g\n", i,j,cluster[i],cluster[j],E,s,E*E/s;
-		    cs+=E*E/s
-		}
-	    };
-	printf "totalEdges %d TE %d cs %g cs/TE %g cs/totalEdges %g\n",totalEdges,TE,cs,cs/TE,cs/totalEdges
-    }' "$@"
-    #/tmp/mp60s/dir-init/AThaliana-CElegans-DMelanogaster-HSapiens-MMusculus-RNorvegicus-SCerevisiae-SPombe.align networks/[A-Z][A-Z][a-z]*/*.el
