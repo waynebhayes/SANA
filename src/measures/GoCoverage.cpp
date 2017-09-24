32130048f226afb014014d1f6e1960e57e6b9f3b
Wayne Hayes <whayes@uci.edu>
2017-09-24 12:59:45 -0700

    renamed some functions and added sqrt option
    Former-commit-id: 91bcc361f3eb61a163ac0bd3d91a8ec352bded72

--- src/measures/GoCoverage.cpp
+++ src/measures/GoCoverage.cpp
@@ -8,9 +8,15 @@
 
 using namespace std;
 
+#define COMBIN 0
+#define INVERSE_FREQ 1
+#define INVERSE_SQRT 0
+#define INVERSE_LOG 0
+
 GoCoverage::GoCoverage(Graph* G1, Graph* G2) :
     Measure(G1, G2, "gocov") {
     NORMALIZE = true;
+    assert(COMBIN + INVERSE_FREQ + INVERSE_LOG + INVERSE_SQRT == 1);
 }
 
 GoCoverage::~GoCoverage() {
@@ -28,26 +34,24 @@ double GoCoverage::Permutation(uint M, uint N) {
     return res;
 }
 
-#define COMBO_GO 0
-#define INVERSE_FREQ_GO 1
-#define INVERSE_LOG_GO 0
-double GoCoverage::ScoreForOneGOterm(uint M, uint N) {
+double GoCoverage::GOtermValuePerAlignment(uint M, uint N) {
     assert(0 < N && N <= M);
-#if COMBO_GO
-    return 1.0/Permutation(M,N);
-#elif INVERSE_FREQ_GO
-    return 1.0; // /min(M,N);
-#elif INVERSE_LOG_GO
-    return 1.0/log(max(M,N)+1);
+#if COMBIN
+    return 1.0/Permutation(M,N); // spread 1 unit across all alignments that match N instances with M.
+#elif INVERSE_FREQ
+    return 1.0; // The GO term gets one unit total for any one alignment.
+#elif INVERSE_SQRT
+    return 1.0/sqrt(N);
+#elif INVERSE_LOG
+    return 1.0/log(N+1);
+#else
+    assert(false);
 #endif
 }
 
-double GoCoverage::downweightedScore(uint M, uint N) {
-#if COMBO_GO
-    return ScoreForOneGOterm(M, N)/N; // divide by M if you want to enforce max only if #GO is same in both networks
-#elif INVERSE_FREQ_GO
-    return ScoreForOneGOterm(M, N)/N; // divide by M if you want to enforce max only if #GO is same in both networks
-#endif
+double GoCoverage::GOtermValuePerAlignedPair(uint M, uint N) {
+    assert(0 < N && N <= M);
+    return GOtermValuePerAlignment(M, N)/N; // divide by M if you want to enforce max only if #GO is same in both networks
 }
 
 double GoCoverage::scoreUpperBound() {
@@ -60,7 +64,7 @@ double GoCoverage::scoreUpperBound() {
         uint N = goCountG2[goTerm];
 	if(N>M) swap(N,M);
         if (N > 0) {
-            total += ScoreForOneGOterm(M, N);
+            total += GOtermValuePerAlignment(M, N);
         }
     }
     return total;
@@ -73,10 +77,10 @@ double GoCoverage::eval(const Alignment& A) {
     vector<vector<uint> > goTermsG2 = GoSimilarity::loadGOTerms(*G2, 1);
     unordered_map<uint,uint> goCountG1 = GoSimilarity::getGoCounts(*G1);
     unordered_map<uint,uint> goCountG2 = GoSimilarity::getGoCounts(*G2);
-
+#if 0
     map<ushort,string> namesG1 = G1->getIndexToNodeNameMap();
     map<ushort,string> namesG2 = G2->getIndexToNodeNameMap();
-
+#endif
     double total = 0;
     for (uint i = 0; i < n1; i++) {
         for (uint goTerm : goTermsG1[i]) {
@@ -84,7 +88,7 @@ double GoCoverage::eval(const Alignment& A) {
                 uint M = goCountG1[goTerm];
                 uint N = goCountG2[goTerm];
                 if (N > M) swap(N, M);
-                if(N>0) total += downweightedScore(M, N);
+                if(N>0) total += GOtermValuePerAlignedPair(M, N);
             }
         }
     }
