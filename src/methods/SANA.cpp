a1e18128d8c646390962ca496db116040612d537
Pasha Khosravi <khosravipasha@gmail.com>
2017-11-10 19:26:39 -0800

    fixed problem with locking crash
    Former-commit-id: 7d25eebf1fa337d78de10d46934c27ac7c564e13

--- src/methods/SANA.cpp
+++ src/methods/SANA.cpp
@@ -214,13 +214,14 @@ Alignment SANA::getStartingAlignment(){
     Alignment randomAlig;
     if (G1->hasNodeTypes()) {
         randomAlig = Alignment::randomAlignmentWithNodeType(G1,G2);
+    	randomAlig.reIndexBefore_Iterations(G1->getNodeTypes_ReIndexMap());
     } else if (lockFileName != "") {
         randomAlig = Alignment::randomAlignmentWithLocking(G1,G2);
+	randomAlig.reIndexBefore_Iterations(G1->getLocking_ReIndexMap());
     } else {
         return Alignment::random(n1, n2);
     }
 
-    randomAlig.reIndexBefore_Iterations(G1->getNodeTypes_ReIndexMap());
     return randomAlig;
 }
 
