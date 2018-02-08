f72d9564cf0639be719573c7286d9907348b2c22
Bisou <Lisera@users.noreply.github.com>
2018-02-07 21:06:34 -0800

    temporary fix

--- src/arguments/ArgumentParser.cpp
+++ src/arguments/ArgumentParser.cpp
@@ -128,9 +128,8 @@ void ArgumentParser::initParsedValues(vector<string> vArg) {
             i = i+k+1;
         }
         else {
-			if (arg.size() > 1) {
-				throw runtime_error("Unknown argument: " + arg + ". See the README for the correct syntax");
-			}
+            if (arg.size() > 1)
+                throw runtime_error("Unknown argument: " + arg + ". See the README for the correct syntax");
         }
         ++i;
     }
