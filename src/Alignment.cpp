af21ad7bd8f8770cd591b402c2b45e1348e07d13
William W <justinianxii@gmail.com>
2018-03-22 10:27:41 +0000

    Merged in Bisou/sana2 (pull request #27)
    Actual errors use cerr
    
    * Serialization pt. 1
    
    * Serialization pt. 1.1
    
    * Serialization 1.3
    
    * Test fix
    
    * Close ofstream
    
    * added ICS as an optimizable measure
    
    * reset LOG10_LOW_TEMP back to -10 where it should be
    
    * LOG10_LOW and LOG10_HIGH_TEMP are now determined automatically
    
    * Complete graph serialization support (cereal)
    
        - Loading a serialized binary versus loading normally can improve graph initialization speed by up to a factor of 20
        - Serialized graph binaries can be found in each species local autogenerated directory
        - The serialized graph will only be used if it's newer than its corresponding el/gw graph
        - If the el/gw graph is newer than the serialized version, SANA will load the el/gw graph normally and generalize a new serialized binary for subsequent use
        - Serialized graphs are affected by the "-locked" and "-nodesHaveTypes" arguments and make different versions based on those arguments
    
    * Cleanup
    
    * Updated makefile
    
    * Separate weighted & unweighted serializations
    
    * Possible bug fix
    
    * Spacing
    
    * Serialization bugs fixed
    
    * Bug fixes
    
    * Spacing
    
    * conflict resolved
    
    * Changed all instances of cerr to cout
    
    * Typo
    
    * conflicts resolved
    
    * cerr to cout
    
    * changed buffering
    
    * changed actual error/warning messages to use cerr
        other messages use cout

--- src/Alignment.cpp
+++ src/Alignment.cpp
@@ -313,14 +313,15 @@ void Alignment::printDefinitionErrors(const Graph& G1, const Graph& G2) {
 
     vector<bool> G2AssignedNodes(n2, false);
     int count = 0;
-    if (A.size() != n1) cout<<"Incorrect size: "<<A.size()<<", should be "<<n1<<endl;
+    if (A.size() != n1) 
+        cerr << "Incorrect size: "<<A.size()<<", should be "<<n1<<endl;
     for (uint i = 0; i < n1; i++) {
         if (A[i] < 0 or A[i] >= n2) {
-            cout<<count<<": node "<<i<<" ("<<G1Names[i]<<") maps to "<<A[i]<<", which is not in range 0..n2 ("<<n2<<")"<<endl;
+            cerr<<count<<": node "<<i<<" ("<<G1Names[i]<<") maps to "<<A[i]<<", which is not in range 0..n2 ("<<n2<<")"<<endl;
             count++;
         }
         if (G2AssignedNodes[A[i]]) {
-            cout<<count<<": node "<<i<<" ("<<G1Names[i]<<") maps to "<<A[i]<<" ("<<G2Names[A[i]]<<"), which is also mapped to by a previous node"<<endl;
+            cerr<<count<<": node "<<i<<" ("<<G1Names[i]<<") maps to "<<A[i]<<" ("<<G2Names[A[i]]<<"), which is also mapped to by a previous node"<<endl;
             count++;
         }
         G2AssignedNodes[A[i]] = true;
