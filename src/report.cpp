94918bd87ea8597f03085d89b03200132c9c217e
Wayne Hayes <whayes@uci.edu>
2018-09-04 14:02:17 -0700

    added compile-time option to print CCS_0 to the .out file

--- src/report.cpp
+++ src/report.cpp
@@ -70,25 +70,34 @@ void makeReport(const Graph& G1, Graph& G2, const Alignment& A,
       table[1][6] = to_string(M.eval("ics",A)); table[1][7] = to_string(M.eval("s3",A));
 
       for (int i = 0; i < tableRows-2; i++) {
-    const vector<uint>& nodes = CS.getConnectedComponents()[i];
-    Graph H = CS.nodeInducedSubgraph(nodes);
-    Alignment newA(nodes);
-    newA.compose(A);
-    table[i+2][0] = "CCS_"+to_string(i); table[i+2][1] = to_string(H.getNumNodes());
-    table[i+2][2] = to_string(H.getNumEdges());
-    table[i+2][3] = to_string(newA.numAlignedEdges(H, G2));
-    table[i+2][4] = to_string(G2.numNodeInducedSubgraphEdges(newA.getMapping()));
-    EdgeCorrectness ec(&H, &G2);
-    table[i+2][5] = to_string(ec.eval(newA));
-    InducedConservedStructure ics(&H, &G2);
-    table[i+2][6] = to_string(ics.eval(newA));
-    SymmetricSubstructureScore s3(&H, &G2);
-    table[i+2][7] = to_string(s3.eval(newA));
+	const vector<uint>& nodes = CS.getConnectedComponents()[i];
+	Graph H = CS.nodeInducedSubgraph(nodes);
+	Alignment newA(nodes);
+	newA.compose(A);
+	table[i+2][0] = "CCS_"+to_string(i); table[i+2][1] = to_string(H.getNumNodes());
+	table[i+2][2] = to_string(H.getNumEdges());
+	table[i+2][3] = to_string(newA.numAlignedEdges(H, G2));
+	table[i+2][4] = to_string(G2.numNodeInducedSubgraphEdges(newA.getMapping()));
+	EdgeCorrectness ec(&H, &G2);
+	table[i+2][5] = to_string(ec.eval(newA));
+	InducedConservedStructure ics(&H, &G2);
+	table[i+2][6] = to_string(ics.eval(newA));
+	SymmetricSubstructureScore s3(&H, &G2);
+	table[i+2][7] = to_string(s3.eval(newA));
       }
 
       stream << "Common connected subgraphs:" << endl;
       printTable(table, 2, stream);
       stream << endl;
+#define PRINT_CCS_0 0
+#if PRINT_CCS_0
+	const vector<uint>& nodes = CS.getConnectedComponents()[0];
+	Graph H = CS.nodeInducedSubgraph(nodes);
+	unordered_map<uint,string> mapG1 = G1.getIndexToNodeNameMap();
+	unordered_map<uint,string> mapG2 = G2.getIndexToNodeNameMap();
+	stream << "CCS_0 Alignment\n";
+	for(uint i=0; i<nodes.size(); i++) stream << mapG1[i] << '\t' << mapG2[A[i]] << endl;
+#endif
       cout << "  printing tables done (" << T2.elapsedString() << ")" << endl;
   }
 }
