b219d1d68aa61cfe4d3cf83d99ac7e4c3be1c363
Wayne Hayes <whayes@uci.edu>
2019-03-05 15:05:52 -0800

    minor mods to regression tests, added code for more core scores

--- regression-tests/EdgeDifference/test.sh
+++ regression-tests/EdgeDifference/test.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
+die() { echo "$@" >&2; exit 1
+}
 
 echo 'Testing measurement Edge Difference'
 
-CURRENT_DIR=`pwd`/regression-tests/EdgeDifference
+TEST_DIR=`pwd`/regression-tests/EdgeDifference
+[ -d "$TEST_DIR" ] || die "should be run from top-level directory of the SANA repo"
+
+exitCode=0
 
 for network in 150 WMean_con WMean_ocd; do
-    file="$CURRENT_DIR/$network"
+    file="$TEST_DIR/$network"
 
     # Run SANA to align the graph to itself
     echo "Aligning network $network"
-    ./sana -t 1 -fg1 "$file.elw" -fg2 "$file.elw" -ed 1 -s3 0 -o "$file" &> "$file.progress"
+    ./sana -t 1 -fg1 "$file.elw" -fg2 "$file.elw" -s3 0 -ed 1 -o "$file" &> "$file.progress"
 
     # Test if SANA has aligned the graph to itself
     match_nodes=`awk '$1==$2' $file.align | wc -l`
-    nodes_count=`awk '{ print $1"\n"$2 }' $file.elw | sort | uniq | wc -l`
+    nodes_count=`awk '{ print $1"\n"$2 }' $file.elw | sort -u | wc -l`
+    echo "  EXPECTED matching nodes: $nodes_count GOT: $match_nodes"
     if (( $match_nodes != $nodes_count )); then
         echo "Failed to align $network to itself" 
-        echo "  EXPECTED matching nodes: $nodes_count GOT: $match_nodes"
-        exit 1
+        exitCode=1
     fi
-    
-    # Remove files if test is passed for this network.
-    for file in $file.align $file.out $file.progress; do
-      rm $file
-    done 
 done
 
-echo 'Testing measurement Edge Difference passed.'
+echo 'Done testing measurement Edge Difference.'
+exit $exitCode
