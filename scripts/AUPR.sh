ee3099e7b8dd7654c3c47ba1f68fb9e8fccf3858
Wayne Hayes <whayes@uci.edu>
2019-04-01 15:30:19 -0700

    updated AUPR.sh

--- scripts/AUPR.sh
+++ scripts/AUPR.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
-TMP=/tmp/ROC.$$
-trap "/bin/rm -f $TMP; exit" 0 1 2 3 15
-USAGE="USAGE: $0 [-AllOrtho 0|1 {default 0}] [-R Resnik threshold] [-S seq thresh] {2 score columns to evaluate (2nd should be 0 to exclude it)} {2-column truth file, eg pairs of orthologs} {G1 edgeList or /dev/null} {G2 edgeList or /dev/null} {p1 p2 2-or-more-scores file} [resnik file] [seqSim File] [complexes file]
-The -AllOrtho flag means compute the AUPR with respect to *all* given orthologs, rather than only those in the scores file"
+TMP=/tmp/AUPR.$$
+trap "/bin/rm -f $TMP.*; exit" 0 1 2 3 15
+USAGE="USAGE:
+
+$0 [-A 0|1] [-R RT] [-S ST] c1 c2 TruthFile G1.el G2.el ScoreFile [resnikFile] [seqSimFile] [complexesFile]
+
+where:
+
+-A [0 or 1] specifies whether or to measure correctness relative to all orthologs or only those found in the scores file
+RT is the Resnik threshold, above which an aligned pair is considered correct even if it is not in the TruthFile
+ST is a sequence threshold, above which an aligned pair is considered correct even if it is not in the TruthFile
+c1 and c2 are the columns in the score file to evaluate (either can be 0 to specify none)
+TruthFile is the file of correct matches
+G1.el and G2.el are edge lists for the networks; they are not mandatory and can be /dev/null
+Scorefile is the file of SANA Core Scores, format is: p1 p2 followed by a list of scores
+resnikFile: file used in association with the -R option
+seqSim File: file to be used in association with the -S option
+complexesFile: CORUM file with list of protein complexes"
+
 die() { echo "$USAGE" >&2; echo "FATAL ERROR: $@" >&2; exit 1;
 }
+
 R=1e30
 S=1e30
 AllOrtho=0
@@ -12,7 +28,7 @@ while [ $# -gt 0 ]; do
 	case $1 in
 	-R) R=$2; shift 2 ;;
 	-S) S=$2; shift 2 ;;
-	-A*) AllOrtho=$2; shift 2;;
+	-A) AllOrtho=$2; shift 2;;
 	*) break ;;
 	esac
 done
@@ -20,12 +36,13 @@ done
 c1=$1
 c2=$2
 shift 2
+[ $# -ge 4 ] || die "not enough arguments"
+
 if [ $c1 -le 0 ]; then c1=$c2; c2=0; fi # swap them if c1 is not needed
-[ $c1 -le 0 ] && die "at least one column must be > 0"
+[ $c1 -le 0 ] && die "at least one of c1 and c2 must be > 0"
 if [ $c1 -gt 0 -a $c1 -lt 3 ]; then die "cannot evaluate column $c1, that contains protein names"; fi
 if [ $c2 -gt 0 -a $c2 -lt 3 ]; then die "cannot evaluate column $c2, that contains protein names"; fi
 
-[ $# -ge 4 ] || die "$USAGE"
 
 hawk '
     function EvalScores(c,thresh){TP=TN=FP=FN=0;
@@ -42,7 +59,7 @@ hawk '
 	TPR=TP/(TP+FN)
 	FPR=FP/(FP+TN)
 	printf " %6d %7d %6d %7d %.3f %.3f %.3f %.3f %.3f",TP,FP,FN,TN,Prec,Rec,F1,TPR,FPR
-	printf " %6d %7d %6d %7d %.3f %.3f %.3f %.3f %.3f",TP,FP,FN,TN,Prec,Rec,F1,TPR,FPR > "'$TMP'"
+	printf " %6d %7d %6d %7d %.3f %.3f %.3f %.3f %.3f",TP,FP,FN,TN,Prec,Rec,F1,TPR,FPR > "'$TMP.AUPR'"
     }
 
     BEGIN{c1='$c1';c2='$c2'
@@ -78,8 +95,8 @@ hawk '
 	}
     }
 
-    ARGIND==5&&!/None/{ R[$2][$3]=1*$4 } # Resnik score file
-    ARGIND==6{ S[$1][$2]=1*$3 } # Sequence sims file
+    ARGIND==5&&!/None/&&($2 in score && $3 in score[$2]){ R[$2][$3]=1*$4 } # Resnik score file
+    ARGIND==6&&($1 in score && $2 in score[$1]){ S[$1][$2]=1*$3 } # Sequence sims file
     ARGIND==7{for(i=1;i<=NF;i++)complex[$i]=complex[$i]" "$0} # Complexes file
     END{
 	delete D1; delete D2; # do not need degrees any more
@@ -95,9 +112,9 @@ hawk '
 	for(c=1;c<=length(cols);c++)EvalScores(c,StatMean(c));
 	print""
 	for(c=1;c<=length(cols);c++){AUPR[c]=0;prevRec[c]=1;prevPrec[c]=0}
-	for(thresh=1e-6;thresh<1.01;thresh+=MIN(thresh/2,.05)){
+	for(thresh=1e-5;thresh<1.01;thresh+=MIN(thresh/2,.05)){
 	    printf "%.6f",thresh
-	    printf "%.6f",thresh > "'$TMP'"
+	    printf "%.6f",thresh > "'$TMP.AUPR'"
 	    done=0;
 	    for(c=1;c<=length(cols);c++){
 		scaledThresh=scale[c]*thresh;EvalScores(c,scaledThresh) # this sets Prec and Rec
@@ -107,7 +124,7 @@ hawk '
 		if(Prec==1&&Rec==0)done++;
 	    }
 	    print""
-	    print "" > "'$TMP'"
+	    print "" > "'$TMP.AUPR'"
 	    if(done == length(cols)) break;
 	}
     }' "$@"
@@ -116,6 +133,6 @@ hawk '
 cols=6
 if [ $c1 -ne 0 -a $c2 -ne 0 ]; then cols="$cols 15"; fi
 for c in $cols; do
-    sort -k ${c}g $TMP | hawk 'BEGIN{colName[6]='$c1';colName[15]='$c2';prevRec=1}/^0/{prec=$'$c';rec=$(1+'$c');h=prec-prevPrec;prevPrec=prec;AUPR+=h*((rec+prevRec)/2);prevRec=rec}END{printf "AUPR[%d] = %g\t", colName['$c'],AUPR}'
+    sort -k ${c}g $TMP.AUPR | hawk 'BEGIN{colName[6]='$c1';colName[15]='$c2';prevRec=1}/^0/{prec=$'$c';rec=$(1+'$c');h=prec-prevPrec;prevPrec=prec;AUPR+=h*((rec+prevRec)/2);prevRec=rec}END{printf "AUPR[%d] = %g\t", colName['$c'],AUPR}'
 done
 echo "" # Final newline after both AUPRs are printed
