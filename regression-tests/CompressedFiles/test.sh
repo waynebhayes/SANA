706781b13793debbfdb44875cc00706847c210fe
Wayne Hayes <whayes@uci.edu>
2019-04-22 10:27:59 -0700

    moved test output to CompressedFiles directory

--- regression-tests/CompressedFiles/test.sh
+++ regression-tests/CompressedFiles/test.sh
@@ -7,14 +7,14 @@ echo 'Testing measurement CompressedFiles'
 TEST_DIR=`pwd`/regression-tests/CompressedFiles
 [ -d "$TEST_DIR" ] || die "should be run from top-level directory of the SANA repo"
 
-echo "" > test1.align
-echo "" > test2.align
+echo "" > $TEST_DIR/test1.align
+echo "" > $TEST_DIR/test2.align
 
 exitCode=0
 
 echo "Test 1: Testing compressed edgelists AThaliana.el.bz2 and MMusculus.el.gz"
-./sana -t 1 -fg1 "$TEST_DIR/AThaliana.el.bz2" -fg2 "$TEST_DIR/MMusculus.el.gz"  -o "test1" &> "test1.progress"
-lines=`wc -l test1.align | awk '{print $1}'`;
+./sana -t 1 -fg1 "$TEST_DIR/AThaliana.el.bz2" -fg2 "$TEST_DIR/MMusculus.el.gz"  -o "$TEST_DIR/test1" &> "$TEST_DIR/test1.progress"
+lines=`wc -l $TEST_DIR/test1.align | awk '{print $1}'`;
 if [ $lines -lt 100 ]
 then
     echo "Test 1: Failed to load networks AThaliana.el.bz2 and MMusculus.el.gz"
@@ -23,8 +23,8 @@ fi
 
 
 echo "Test 2: Testing compressed edgelists AThaliana.el.gz and yeast-reduced-wayne.gw.gz"
-./sana -t 1 -fg1 "$TEST_DIR/AThaliana.el.gz" -fg2 "$TEST_DIR/yeast-reduced-wayne.gw.gz"  -o "test2" &> "test2.progress"
-lines=`wc -l test1.align | awk '{print $1}'`;
+./sana -t 1 -fg1 "$TEST_DIR/AThaliana.el.gz" -fg2 "$TEST_DIR/yeast-reduced-wayne.gw.gz"  -o "$TEST_DIR/test2" &> "$TEST_DIR/test2.progress"
+lines=`wc -l $TEST_DIR/test1.align | awk '{print $1}'`;
 if [ $lines -lt 100 ]
 then
     echo "Test 2: Failed to load networks AThaliana.el.gz and yeast-reduced-wayne.gw.gz"
