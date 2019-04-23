2320e11c4eed0eed18ccd1baf3b60d82ec2c220c
Wayne Hayes <whayes@uci.edu>
2019-04-23 07:54:25 -0700

    added progress to regression tests

--- regression-test-all.sh
+++ regression-test-all.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 RETURN=0
 for dir in regression-tests/*; do
+    echo --- in directory $dir ---
     for r in $dir/*.sh; do
+	echo --- running test $r ---
 	if nice -19 "$r"; then
 	    :
 	else
