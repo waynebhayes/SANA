061e107129b9c280e02105cb462776d686506caf
howboutn0 <josephxu1234@gmail.com>
2019-06-05 16:33:06 -0700

    Create GraphletNorm.hpp
    created header file for graphletnorm

---
+++ src/measures/GraphletNorm.hpp
@@ -0,0 +1,22 @@
+#ifndef GRAPHLETNORM_HPP
+#define GRAPHLETNORM_HPP
+#include "LocalMeasure.hpp"
+
+class GraphletNorm: public LocalMeasure {
+public:
+    GraphletNorm(Graph* G1, Graph* G2);
+    virtual ~GraphletNorm();
+
+private:
+    void initSimMatrix();
+    double magnitude(vector<uint> vector);
+    const uint NUM_ORBITS = 73;
+    vector<double> NODV(vector<uint> v);
+    double ODVratio(vector<double> u, vector<double> v, uint i);
+    double ODVdiff(vector<uint> u, vector<uint> v);
+    double ODVsim(vector<uint> u, vector<uint> v);
+    vector<uint> reduce(vector<uint> v);
+    
+};
+
+#endif
