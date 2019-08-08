4a90efe59b1c1a10ff404cdfe21f6d24facf945f
Wayne Hayes <whayes@uci.edu>
2019-08-07 17:54:50 -0700

    rm /tmp/syeast in multi test

--- regression-tests/multi-pairwise/test.sh
+++ regression-tests/multi-pairwise/test.sh
@@ -1,1 +1,4 @@
-./multi-pairwise.sh ./sana.multi '-s3 0 -ses 1' 10 1 '-parallel 4' /tmp/syeast networks/syeast[12]?/*.el
+#!/bin/sh
+DIR=/tmp/syeast
+/bin/rm -rf $DIR
+./multi-pairwise.sh ./sana.multi '-s3 0 -ses 1' 10 1 '-parallel 4' $DIR networks/syeast[12]?/*.el
