421e56ab73f7dc00c0e989f7cb5a240449c4e345
Wayne Hayes <whayes@uci.edu>
2019-08-28 11:28:01 -0700

    added MNC.sh

---
+++ scripts/MNC.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+USAGE="MNC=Multi-Node-Correctness"
+TMP=/tmp/MNC.$$
+trap "/bin/rm -f $TMP; exit" 0 1 2 3 15
+
+cat "$@" | sort -S1G | uniq -c | awk ' { delete K; for(i=2;i<=NF;i++)++K[$i];delete K["_"];biggest=0;for(i in K)if(K[i]>biggest)biggest=K[i];print $1,biggest}' > $TMP & # get this running so pearson will be fast later
+
+for i
+do
+    echo -n "$i	"
+awk '{
+	delete K
+	for(i=1;i<=NF;i++)++K[$i]
+	biggest=0; # note "_" is an empty column
+	for(i in K)if(i!="_" && K[i]>biggest)biggest=K[i]
+	columns=NF-K["_"]
+	if(biggest>1) {
+	    sumNC+=biggest/columns
+	    wSumNC+=biggest
+	}
+	weight+=columns
+    }
+    END{printf "%d\t%.3f\n",NR,wSumNC/weight}' "$i"
+done
+echo "Pearson of core frequency-vs-biggest: "
+wait
+pearson < $TMP
