93f1e991589d4e4d22128e2782d7d80012994689
Wayne Hayes <whayes@uci.edu>
2019-09-10 12:07:31 -0700

    homologs.sh seems to work now, except still expects taxons, not names

--- scripts/homologs.sh
+++ scripts/homologs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-USAGE="$0 [-c] [-1 species1 species2] column1 column2 [list of files]"
+USAGE="$0 [-c] [-1] species1 species2 column1 column2 [list of files]"
 die() { echo "$USAGE" >&2; echo "$@" >&2; exit 1
 }
 one2one=0
@@ -9,17 +9,16 @@ COUNT=0
 while :; do
     case $1 in
     -c) COUNT=1; shift;;
-    -1) one2one=1
-	s1=$2
-	s2=$3
-	shift 3;;
+    -1) one2one=1; shift;;
     *) break ;;
     esac
 done
-[ $# -ge 2 ] || die "wrong numebr of arguments"
-col1=$1
-col2=$2
-shift 2
+[ $# -ge 4 ] || die "wrong numebr of arguments"
+s1=$1
+s2=$2
+col1=$3
+col2=$4
+shift 4
 
 STDIN=""
 if [ $# -eq 0 ]; then STDIN='-'; fi # if there are no arguments, then should read the standard input
@@ -28,12 +27,8 @@ awk 'BEGIN{o='$one2one';s1='$s1';s2='$s2';c1='$col1';c2='$col2'}
     ARGIND==1{geneHIDs[$3][$1][$2]++;HIDgenes[$1][$3][$2]++;HIDspec[$1][$2]++}
     ARGIND>1{if($c1 in geneHIDs)for(H in geneHIDs[$c1])if($c2 in HIDgenes[H] && $c2 in geneHIDs && H in geneHIDs[$c2])
 	{
-	    if(!o){
-		printf "%s\t%s",$c1,$c2
-		if('$COUNT')for(S1 in geneHIDs[$c1][H])for(S2 in geneHIDs[$c2][H])
-		    printf "\t[(%s %d),(%s %d)]",S1,HIDspec[H][S1],S2,HIDspec[H][S2]
-	    }
-	    else if(s1 in geneHIDs[$c1][H] && s2 in geneHIDs[$c2][H] && HIDspec[H][s1]==1 && HIDspec[H][s2]==1)
+	    if(s1 in geneHIDs[$c1][H] && s2 in geneHIDs[$c2][H] &&
+		(!o || (o && HIDspec[H][s1]==1 && HIDspec[H][s2]==1)))
 	    {
 		printf "%s\t%s",$c1,$c2
 		if('$COUNT')for(S1 in geneHIDs[$c1][H])for(S2 in geneHIDs[$c2][H])
