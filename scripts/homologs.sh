59606af8acde6ca03a37a15751203a4165b13b7b
Wayne Hayes <whayes@uci.edu>
2019-09-11 08:30:51 -0700

    I think homologs.sh now works

--- scripts/homologs.sh
+++ scripts/homologs.sh
@@ -1,34 +1,52 @@
 #!/bin/sh
-USAGE="$0 [-c] [-1] species1 species2 column1 column2 [list of files]"
+USAGE="$0 [-c] [-1] species1 species2 column1 column2 [list of files]
+or
+$0 [-a] [-1] species [species2]: to just list all the homologs of either 1 or 2 species
+or
+"
 die() { echo "$USAGE" >&2; echo "$@" >&2; exit 1
 }
 one2one=0
-s1=0
-s2=0
+tax1=0
+tax2=0
 COUNT=0
+ALL=0
+col1=0
+col2=0
+
 while :; do
     case $1 in
+    -a) ALL=1; shift ;;
     -c) COUNT=1; shift;;
     -1) one2one=1; shift;;
     *) break ;;
     esac
 done
-[ $# -ge 4 ] || die "wrong numebr of arguments"
-s1=$1
-s2=$2
-col1=$3
-col2=$4
-shift 4
+
+if [ "$ALL" = 1 -a $# -eq 1 ]; then
+    tax=`BioGRIDname $1 | cut -f3`
+    awk '$2=='$tax'{print $3}' /home/sana/sequence/homologene/homologene.tsv | sort -u
+    exit
+fi
+tax1=`BioGRIDname $1 | cut -f3`
+tax2=`BioGRIDname $2 | cut -f3`
+shift 2
+if [ "$ALL" != 1 ]; then
+    [ $# -ge 2 ] || die "wrong numebr of arguments"
+    col1=$1
+    col2=$2
+    shift 2
+fi
 
 STDIN=""
-if [ $# -eq 0 ]; then STDIN='-'; fi # if there are no arguments, then should read the standard input
+if [ $# -eq 0 -a ! "$ALL" ]; then STDIN='-'; fi # if there are no arguments, then should read the standard input
 
-awk 'BEGIN{o='$one2one';s1='$s1';s2='$s2';c1='$col1';c2='$col2'}
+awk 'BEGIN{o='$one2one';tax1='$tax1';tax2='$tax2';c1='$col1';c2='$col2'}
     ARGIND==1{geneHIDs[$3][$1][$2]++;HIDgenes[$1][$3][$2]++;HIDspec[$1][$2]++}
     ARGIND>1{if($c1 in geneHIDs)for(H in geneHIDs[$c1])if($c2 in HIDgenes[H] && $c2 in geneHIDs && H in geneHIDs[$c2])
 	{
-	    if(s1 in geneHIDs[$c1][H] && s2 in geneHIDs[$c2][H] &&
-		(!o || (o && HIDspec[H][s1]==1 && HIDspec[H][s2]==1)))
+	    if(tax1 in geneHIDs[$c1][H] && tax2 in geneHIDs[$c2][H] &&
+		(!o || (o && HIDspec[H][tax1]==1 && HIDspec[H][tax2]==1)))
 	    {
 		printf "%s\t%s",$c1,$c2
 		if('$COUNT')for(S1 in geneHIDs[$c1][H])for(S2 in geneHIDs[$c2][H])
@@ -36,5 +54,18 @@ awk 'BEGIN{o='$one2one';s1='$s1';s2='$s2';c1='$col1';c2='$col2'}
 	    }
 	    else next;
 	    print ""
+	}
+    }
+    END{
+	if('$ALL') for(c1 in geneHIDs)for(H in geneHIDs[c1])for(c2 in HIDgenes[H])if(c2 in geneHIDs && H in geneHIDs[c2])
+	{
+	    if(tax1 in geneHIDs[c1][H] && tax2 in geneHIDs[c2][H] &&
+		(!o || (o && HIDspec[H][tax1]==1 && HIDspec[H][tax2]==1)))
+	    {
+		printf "%s\t%s",c1,c2
+		if('$COUNT')for(S1 in geneHIDs[c1][H])for(S2 in geneHIDs[c2][H])
+		    printf "\t[(%s %d),(%s %d)]",S1,HIDspec[H][S1],S2,HIDspec[H][S2]
+		print ""
+	    }
 	}
     }' /home/sana/sequence/homologene/homologene.tsv $STDIN "$@"
