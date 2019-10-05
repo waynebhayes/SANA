1c956bd72797b2f06a972e421574355adc06d250
Wayne Hayes <whayes@uci.edu>
2019-10-04 22:59:27 -0700

    trap multi-pairwise to remove TMP dir, and increase EdgeDiff to 10 minutes

--- regression-tests/multi-pairwise/test.sh
+++ regression-tests/multi-pairwise/test.sh
@@ -5,7 +5,7 @@ DIR=/tmp/syeast.$$
 EXIT_CODE=0
 MINSUM=0.4
 MEASURE=ms3
-#trap "/bin/rm -rf $DIR" 0 1 2 3 15
+trap "/bin/rm -rf $DIR" 0 1 2 3 15
 if [ `hostname` = Jenkins ]; then
     ITERS=99; minutes=1
 else
