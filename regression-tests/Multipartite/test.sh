c4617ba30fd5596ac6472eb6b52946b4495ab03b
waynebhayes <whayes@uci.edu>
2019-11-29 20:27:49 -0800

    bunch of minor changes to fix MacOS regression tests

--- regression-tests/Multipartite/test.sh
+++ regression-tests/Multipartite/test.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
-die() { echo "$@" >&2; exit 1
+die() { /bin/echo "$@" >&2; exit 1
 }
 
-echo 'Testing Multipartite mode'
+/bin/echo -n 'Testing Multipartite mode: '
 
 TEST_DIR=`pwd`/regression-tests/Multipartite
 [ -d "$TEST_DIR" ] || die "should be run from top-level directory of the SANA repo"
 
-
-rm -f $TEST_DIR/sana.align
+rm -f $TEST_DIR/mpel.align $TEST_DIR/mpel.*out
 
 exitCode=0
 
-./sana -s3 1 -t 1 -fg1 "$TEST_DIR/multipartite_test.mpel" -fg2 "$TEST_DIR/multipartite_test_2.mpel" -o "$TEST_DIR/sana" -multipartite 3 || exit
+./sana -s3 1 -t 1 -fg1 "$TEST_DIR/multipartite_test.mpel" -fg2 "$TEST_DIR/multipartite_test_2.mpel" -o "$TEST_DIR/mpel" -multipartite 3 >$TEST_DIR/mpel.stdout 2>&1 || die "sana failed to run"
 
 cd $TEST_DIR
-matches=`awk '{ if ($1 == $2) { print "same"; } }' < sana.align | wc -l`
-lines=`wc -l < sana.align`
+matches=`awk '{ if ($1 == $2) { print "same"; } }' < mpel.align | wc -l`
+lines=`wc -l < mpel.align`
 
-if [ $lines != $matches ]
-then
-    echo "Test failed to align multipartite graphs"
-    exitCode=1
+if [ $lines -eq $matches ]; then
+    echo "multi-partite test passed"
+else
+    die "Test failed to align multipartite graphs"
 fi
-
-exit $exitCode
--- regression-tests/multi-pairwise/test.sh
+++ regression-tests/multi-pairwise/test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo() { /bin/echo "$@"
+}
 PATH="`pwd`/scripts:$PATH"
 export PATH
 DIR=/tmp/syeast.$$
@@ -27,15 +29,15 @@ rename.sh ';dir;dir0;' dir?
 mv dir00 dir0 # all except this zeroth one
 echo "Now check NC values: below are the number of times the multiple alignment contains k correctly matching nodes, k=2,3,4:"
 echo "iter	NC2	NC3	NC4"
-for d in dir??; do echo "$d" `for i in 2 3 4; do awk '{delete K;for(i=1;i<=NF;i++)++K[$i];for(i in K)if(K[i]>='$i')print}' $d/multiAlign.tsv | wc -l; done` | sed 's/ /\t/g'; done
+for d in dir??; do echo "$d" `for i in 2 3 4; do gawk '{delete K;for(i=1;i<=NF;i++)++K[$i];for(i in K)if(K[i]>='$i')print}' $d/multiAlign.tsv | wc -l; done` | sed 's/ /	/g'; done
 echo "And now the Multi-NC, or MNC, measure, of the final alignment"
 echo 'k	number	MNC'
-for k in 2 3 4; do echo "$k	`awk '{delete K;for(i=1;i<=NF;i++)++K[$i];for(i in K)if(K[i]>='$k')nc++}END{printf "%d\t%.3f\n",nc,nc/NR}' dir$ITERS/multiAlign.tsv`"
+for k in 2 3 4; do echo "$k	`gawk '{delete K;for(i=1;i<=NF;i++)++K[$i];for(i in K)if(K[i]>='$k')nc++}END{printf "%d\t%.3f\n",nc,nc/NR}' dir$ITERS/multiAlign.tsv`"
 done | tee $DIR/MNC.txt
 echo "Now check that MNC values are high enough: for k=2,3,4, we want MNC 0.25, 0.15, and 0.05 respectively;
 or at least have the three of them sum to $MINSUM"
 echo "DIR is $DIR"
-if awk 'BEGIN{code=0}{k=$1;expect=(0.45-k/10);sum+=$3;if($3<expect)code=1}END{if(sum>'$MINSUM')code=0; exit(code)}' $DIR/MNC.txt; then
+if gawk 'BEGIN{code=0}{k=$1;expect=(0.45-k/10);sum+=$3;if($3<expect)code=1}END{if(sum>'$MINSUM')code=0; exit(code)}' $DIR/MNC.txt; then
     :
 else
     EXIT_CODE=1
