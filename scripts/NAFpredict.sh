6cfb00dcfea7ef1a17e01f61f0caf3311c92ee3c
Wayne Hayes <whayes@uci.edu>
2020-05-02 08:59:30 -0700

    NAFpredict now outputs -p, -v, and -PIP without having to be run 3 times

--- scripts/NAFpredict.sh
+++ scripts/NAFpredict.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
-USAGE="$0 -[p|v] seqSim go1 go2 tax1 tax2 NAFthresh col1 col2 [Resnik.files]
+#!/bin/bash
+USAGE="$0 net1.el net2.el seqSim go1 go2 tax1 tax2 NAFthresh col1 col2 outName [Resnik.files]
+NOTE: predictions go to outName-p, validations to outName-v, summary to stdout
 where:
-seqSim is the file with sequence similarities used to eliminate predictions possible to predict by sequence
-go1 is the source gene2go file which makes predictions which are searched in go2
+seqSim is the file with sequence similarities/orthologs used to eliminate predictions possible to predict by sequence
+go1 is the basename of source gene2go file which makes predictions which are searched in go2
+    (do not include the allGO or NOSEQ part, that is done automatically)
 tax1 and tax2 are the predictor and predictee taxonomic IDs (eg 10090 for mouse, 9606 for human)
 NAFthresh is the lower predictive bound, and has a leading dash if you want to predict in the opposite direction
     to the directory names (eg 0 to predict MM->HS or -0 for MM<-HS)
@@ -21,30 +23,42 @@ EVC_ALL="EXP HDA HEP HGI HMP IBA IC IDA IEA IEP IGC IGI IKR IMP IMR IPI IRD ISA 
 EVC_NOS="EXP HDA HEP HGI HMP IC IDA IEP IGI IKR IMP IMR IPI IRD NAS ND TAS"
 EVC_SEQ="IBA IEA IGC ISA ISM ISO ISS RCA"
 
-LIST_PRED=false
-LIST_VAL=false
 case "$1" in
--p) LIST_PRED=true;shift;;
--v) LIST_VAL=true;shift;;
 -*) die "unknown option '$1'";;
 esac
 
-[ $# -ge 8 ] || die "expecting at least 8 args, not $#"
-seqSim="$1"
-go1="$2"
-go2="$3"
-tax1=$4
-tax2=$5
-NAFthresh=$6
-c1=$7
-c2=$8
-shift 8
-[ -f "$go1" ] || die "can't find gene2go file '$go1'"
-[ -f "$go2" ] || die "can't find gene2go file '$go2'"
+[ $# -ge 11 ] || die "expecting at least 11 args, not $#"
+G1="$1"
+G2="$2"
+seqSim="$3"
+go1="$4"
+go2="$5"
+tax1=$6
+tax2=$7
+NAFthresh=$8
+c1=$9
+c2=${10}
+outName=${11}
+shift 11
+[ -f $G1 -a -f $G2 ] || die "need network files $G1 and $G2"
+[ -f $outName-v -o -f $outName-p ] && die "refusing to overwrite existing $outName-[pv]"
+for g in allGO NOSEQ; do
+    [ -f "$go1.$g" ] || die "can't find gene2go file '$go1.$g'"
+    [ -f "$go2.$g" ] || die "can't find gene2go file '$go2.$g'"
+done
 
 TMPDIR=/tmp/GOpredict.$$
 trap "/bin/rm -rf $TMPDIR" 0 1 2 3 15
 mkdir $TMPDIR
+echo $EVC_SEQ | newlines | awk '{printf "\t%s\t\n",$0}' > $TMPDIR/EVC_SEQ # sequence evidence codes
+
+# "Predictions" that are already known for the target species at time t1:
+grep "^$tax2	" $go1.allGO | cut -f1-3 | sort -u >$TMPDIR/go1.tax2.already
+# "Predictions" that had been derived via sequence for the target species by time t2:
+grep "^$tax2	" $go2.allGO | fgrep -f $TMPDIR/EVC_SEQ | cut -f1-3 | sort -u >$TMPDIR/go2.tax2.already
+#Predictable-in-principle
+./Predictable.sh $tax1 $tax2 $go1 $go2 $G2 $TMPDIR/EVC_SEQ > $TMPDIR/Predictable
+
 dataDir=`echo "$@" | newlines | sed 's,/[^/]*$,,' | sort -u`
 sort "$@" | uniq -c | sort -nr | awk 'BEGIN{tax1='$tax1';tax2='$tax2';
 	c1=1+'$c1';c2=1+'$c2'; # increment column since "uniq -c" above prepends NAF to the line
@@ -52,7 +66,7 @@ sort "$@" | uniq -c | sort -nr | awk 'BEGIN{tax1='$tax1';tax2='$tax2';
     }
     ARGIND==1{seq[$1][$2]=1;next} # sequence similar pairs
     ARGIND==2{u=$c1;;v=$c2;
-	if(u in seq && v in seq[u])next; # ignore if sequence similar
+	if(u in seq && v in seq[u])next; # ignore known orthology or sequence similarity
 	NAF[u][v]=$1; # store ALL NAFs for now, not just those above the threshold
 	next
     }
@@ -68,45 +82,55 @@ sort "$@" | uniq -c | sort -nr | awk 'BEGIN{tax1='$tax1';tax2='$tax2';
 			# if protein p2 is not listed at all in the gene2go file...
 			#... or if it is but does not have this particular GO term...
 			if(!(p2 in pGO[tax2]) ||!(g in pGO[tax2][p2]))
-			    # it is a prediction!!! Print out the expected line to find in the later gene2go file:
-			    for(evc in pGO[tax1][p1][g])
+			    for(evc in pGO[tax1][p1][g]) {
 				NAFpredict[tax2][p2][g][evc]+=NAF[p1][p2]
+				NAFpredict[tax2][p2][g]["ALL"]+=NAF[p1][p2]
+			    }
 	# Now that we have accumulated all the possible predictions and evidences, print out only those that meet NAFthresh
 	for(p2 in NAFpredict[tax2])for(g in NAFpredict[tax2][p2])for(evc in NAFpredict[tax2][p2][g])
-	    if(NAFpredict[tax2][p2][g][evc] >= NAFthresh) # do we want EACH evidence above NAFthresh, or just the total?
+	    if(NAFpredict[tax2][p2][g][evc] >= NAFthresh) { # do we want EACH evidence above NAFthresh, or just the total?
+		# it is a prediction!!! Print out the NAF and the expected line to find in the later gene2go file:
 		printf "%d\t%d\t%s\t%s\t%s\t%s\n",NAFpredict[tax2][p2][g][evc],tax2,p2,g,evc,H[g]
-    }' "$seqSim" - "$go1" |
+	    }
+    }' "$seqSim" - "$go1.NOSEQ" | # make predictions using ????
+	fgrep -v -f $TMPDIR/go1.tax2.already | # remove ones that are already known in allGO
 	sort -nr | # sort highest first but do NOT remove duplicates: the same p2 predicted from different p1s means something
 	tee $TMPDIR/predictions.allCol |
 	cut -f2-4 | # remove NAF, evidence code &hier from prediction before search
-	sort -u | fgrep -f - "$go2" | fgrep -v '	NOT	' | sort -u > $TMPDIR/validated.allCol
-if $LIST_PRED; then cat $TMPDIR/predictions.allCol # includes leading NAF+duplicates, each should be from a different p1 in tax1
-elif $LIST_VAL; then cat $TMPDIR/validated.allCol
-else
-    evCodesPred=`cut -f5 $TMPDIR/predictions.allCol | sort -u` # col5 because of leading NAF
-    evCodesVal=`cut -f4 $TMPDIR/validated.allCol | sort -u`
-    Categories=`cut -f8 $TMPDIR/validated.allCol | sort -u`
-    # Shorten GO fileNames for output
-    GO1=`echo $go1|sed 's,^.*/go/,,'`
-    GO2=`echo $go2|sed 's,^.*/go/,,'`
-    for evc in $evCodesPred; do
-	pred=`fgrep "	$evc	" $TMPDIR/predictions.allCol | cut -f2-4 | sort -u | tee $TMPDIR/pred$evc | wc -l`
-	val=`fgrep -f $TMPDIR/pred$evc $TMPDIR/validated.allCol | cut -f1-3 | sort -u | tee $TMPDIR/val$evc | wc -l`
-	echo "$evc $pred $val" |
-	    awk '$2>0{printf "%10s %3s NAF %3d : %6d pred %5d val prec %5.1f%%\n",
-		"'$dataDir'",$1,'$NAFthresh',$2,$3,100*$3/$2}'
-    done > $TMPDIR/predECSummary
-    for c in $Categories; do
-	pred=`fgrep "	$c" $TMPDIR/predictions.allCol | cut -f2-4 | sort -u | tee $TMPDIR/pred$c | wc -l`
-	val=`fgrep -f $TMPDIR/pred$c $TMPDIR/validated.allCol | cut -f1-3 | sort -u | tee $TMPDIR/val$c | wc -l`
-	echo "$c $pred $val" |
-	    awk '$2>0{printf "%10s %9s NAF %3d : %6d pred %5d val prec %5.1f%%\n",
-		"'$dataDir'",$1,'$NAFthresh',$2,$3,100*$3/$2}'
-    done > $TMPDIR/predHierSummary
-    echo "Predictions by evidence code for $dataDir $GO1 -> $GO2, NAF $NAFthresh"
-    lastCol=`awk '{print NF}' $TMPDIR/predECSummary | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'`
-    [ "X$lastCol" != X ] && sort -k ${lastCol}gr $TMPDIR/predECSummary
-    echo "Predictions by GO hierarchy for $dataDir $GO1 -> $GO2, NAF $NAFthresh"
-    lastCol=`awk '{print NF}' $TMPDIR/predHierSummary | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'`
-    [ "X$lastCol" != X ] && sort -k ${lastCol}gr $TMPDIR/predHierSummary
-fi
+	sort -u | fgrep -f - "$go2.NOSEQ" | # validate in NOSEQ
+	fgrep -v '	NOT	' | fgrep -v -f $TMPDIR/go2.tax2.already | # remove seq-based discovered by t2
+	sort -u > $TMPDIR/validated.allCol
+
+# Now process
+evCodesPred=`cut -f5 $TMPDIR/predictions.allCol | sort -u` # col5 because of leading NAF
+evCodesVal=`cut -f4 $TMPDIR/validated.allCol | sort -u`
+Categories=`cut -f8 $TMPDIR/validated.allCol | sort -u`
+# Shorten GO fileNames for output
+GO1=`echo $go1|sed 's,^.*/go/,,'`
+GO2=`echo $go2|sed 's,^.*/go/,,'`
+for evc in $evCodesPred; do
+    pred=`fgrep "	$evc	" $TMPDIR/predictions.allCol | cut -f2-4 | sort -u | tee $TMPDIR/pred$evc | wc -l`
+    PIP=`cut -f1-3 $TMPDIR/Predictable | sort -u | fgrep -f - $go2.NOSEQ | fgrep -c "	$evc	"`
+    val=`cut -f1-3 $TMPDIR/Predictable | fgrep -f - $TMPDIR/pred$evc | cut -f1-3 | fgrep -f - $TMPDIR/validated.allCol | cut -f1-3 | sort -u | tee $TMPDIR/val$evc | wc -l`
+    echo "$evc $pred $PIP $val" |
+	awk '$2>0{printf "%10s %3s NAF %3d : %6d pred %6d PIP %5d val prec %5.1f%%\n",
+	    "'$dataDir'",$1,'$NAFthresh',$2,$3,$4,100*$4/$2}'
+done > $TMPDIR/predECSummary
+for c in $Categories; do
+    pred=`fgrep "	$c" $TMPDIR/predictions.allCol | cut -f2-4 | sort -u | tee $TMPDIR/pred$c | wc -l`
+    PIP=`cut -f1-3 $TMPDIR/Predictable | sort -u | fgrep -f - $go2.NOSEQ | fgrep -c "	$c"`
+    val=`fgrep -f $TMPDIR/pred$c $TMPDIR/validated.allCol | cut -f1-3 | sort -u | tee $TMPDIR/val$c | wc -l`
+    echo "$c $pred $PIP $val" |
+	awk '$2>0{printf "%10s %9s NAF %3d : %6d pred %6d PIP %5d val prec %5.1f%%\n",
+	    "'$dataDir'",$1,'$NAFthresh',$2,$3,$4,100*$4/$2}'
+done > $TMPDIR/predHierSummary
+echo "Predictions by evidence code for $dataDir $GO1 -> $GO2, NAF $NAFthresh"
+lastCol=`awk '{print NF}' $TMPDIR/predECSummary | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'`
+[ "X$lastCol" != X ] && sort -k ${lastCol}gr $TMPDIR/predECSummary
+echo "Predictions by GO hierarchy for $dataDir $GO1 -> $GO2, NAF $NAFthresh"
+lastCol=`awk '{print NF}' $TMPDIR/predHierSummary | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'`
+[ "X$lastCol" != X ] && sort -k ${lastCol}gr $TMPDIR/predHierSummary
+
+mv $TMPDIR/predictions.allCol $outName-p # includes leading NAF+duplicates, each should be from a different p1 in tax1
+mv $TMPDIR/Predictable $outName-PIP
+mv $TMPDIR/validated.allCol $outName-v
