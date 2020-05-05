bc1d2ce54b345f07bfb23fb2db613373e2a3fa51
Wayne Hayes <whayes@uci.edu>
2020-05-05 08:11:26 -0700

    minor updates to regressions

--- regression-tests/BioGRID/locking.sh
+++ regression-tests/BioGRID/locking.sh
@@ -11,7 +11,7 @@ PATH="$PATH:`pwd`/scripts"
 export PATH
 
 TMPDIR=/tmp/regression-test$$
-trap "/bin/rm -rf $TMPDIR" 0 1 2 3 15
+#trap "/bin/rm -rf $TMPDIR" 0 1 2 3 15
 mkdir $TMPDIR
 
 OutputFile="regression-test.result"
@@ -40,23 +40,27 @@ export Networks_count=`echo $nets | wc -w`
 NUM_FAILS=0
 NUM_LOCK=100 # number of nodes to lock
 echo -n "Creating random lock files; "
-echo -n > $OutputDir/networks.locking
+echo -n > $TMPDIR/networks.locking
 echo "$nets" |
     awk '{net[NR-1]=$NF}END{for(i=0;i<NR;i++)for(j=i+1;j<NR;j++) printf "%s %s\n",net[i],net[j]}' | while read g1 g2
     do
-	echo $g1 $g2 >> $OutputDir/networks.locking
-	paste <(cat networks/$g1.el | newlines | sort -u | randomizeLines | head -$NUM_LOCK) <(cat networks/$g2.el | newlines | sort -u | randomizeLines | head -$NUM_LOCK) > $OutputDir/$g1-$g2.lock
+	echo $g1 $g2 >> $TMPDIR/networks.locking
+	paste <(cat networks/$g1.el | newlines | sort -u | randomizeLines | head -$NUM_LOCK) <(cat networks/$g2.el | newlines | sort -u | randomizeLines | head -$NUM_LOCK) > $TMPDIR/$g1-$g2.lock
     done
 echo "running lock test."
-echo "$nets" | awk '{net[NR-1]=$NF}END{for(i=0;i<NR;i++)for(j=i+1;j<NR;j++) printf "echo Running %s-%s; ./sana -t 1 -s3 1 -g1 %s -g2 %s -lock '$OutputDir'/%s-%s.lock -o '$OutputDir'/%s-%s > '$OutputDir'/%s-%s.progress 2>&1\n",net[i],net[j],net[i],net[j],net[i],net[j],net[i],net[j],net[i],net[j]}' | eval $PARALLEL
+echo "$nets" | awk '{net[NR-1]=$NF}END{for(i=0;i<NR;i++)for(j=i+1;j<NR;j++) printf "echo Running %s-%s; ./sana -t 1 -s3 1 -g1 %s -g2 %s -lock '$TMPDIR'/%s-%s.lock -o '$TMPDIR'/%s-%s > '$TMPDIR'/%s-%s.progress 2>&1\n",net[i],net[j],net[i],net[j],net[i],net[j],net[i],net[j],net[i],net[j]}' | eval $PARALLEL
 echo "Checking SANA Locking Mechanism" | tee -a $OutputFile
-cat $OutputDir/networks.locking | while read Network1 Network2; do
-    if [[ `grep -c -x -f "$OutputDir/$Network1-$Network2.lock" "$OutputDir/$Network1-$Network2.align"` -ne $NUM_LOCK ]]; then
+cat $TMPDIR/networks.locking | while read Network1 Network2; do
+    if [[ `grep -c -x -f "$TMPDIR/$Network1-$Network2.lock" "$TMPDIR/$Network1-$Network2.align"` -ne $NUM_LOCK ]]; then
 	touch $TMPDIR/failed
-	echo "LOCKING FAILED on $OutputDir/$Network1-$Network2" >&2
+	echo "LOCKING FAILED on $TMPDIR/$Network1-$Network2" >&2
         (( ++NUM_FAILS ))
     fi
 done
 
 echo encountered a total of $NUM_FAILS failures
-exit $NUM_FAILS
+if [ "$NUM_FAILS" -eq 0 ]; then
+    /bin/rm -rf $TMPDIR
+else
+    echo "results in $TMPDIR">&2
+fi
