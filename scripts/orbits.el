4dc5e941fe504eef15db8ab1ee4f14ad79bcd190
Wayne Hayes <whayes@uci.edu>
2020-06-18 22:13:27 -0700

    adding NAUTY-dreadnaut-orbits.el

---
+++ scripts/orbits.el
@@ -0,0 +1,65 @@
+#!/bin/sh
+USAGE="$0 {network.el}
+Purpose: use NAUTY's dreadnaut program to enumerate orbits in a graph represented as an edge list.
+Since NAUTY insists on node names being integers, we map the network's node names to ints and back.
+Output: each line contains exactly one orbit; the line contains all nodes in that orbit.
+Thus, the number of columns is equal to the number of nodes in that orbit.
+Every node in the network will appear exactly once in the output.
+Orbits, and the nodes therein, are listed in no particular order.
+(NAUTY is available on Brendan McKay's web page, with an unofficial copy on github.com)"
+
+# Path the the "dreadnaut" executable
+#DREADNAUT=/scratch/preserve/extra2/preserve/nauty/nauty26r12/dreadnaut
+DREADNAUT=/home/sana/bin/dreadnaut
+
+die() { echo "FATAL ERROR: $@" >&2; exit 1
+}
+[ $# -eq 1 ] || die "expecting exactly one filename, an edgelist"
+
+LANG=C # ensure "sort -n" puts alphas before nums
+NL='
+'
+
+# awk code to convert edge list to integer IDs. We need to use it twice,
+# so to ensure it's exactly the same code, put it in a SHELL variable
+NAME2ID='
+    if(!($1 in id)){name[1*n]=$1;id[$1]=n++}
+    if(!($2 in id)){name[1*n]=$2;id[$2]=n++}
+    edge[$1][$2]=edge[$2][$1]=1'
+
+(
+	awk "ARGIND==1{$NAME2ID}"'
+	END{
+	    printf "n=%d g\n",n; # NAUTY expects the number of nodes and "g" as the first line
+	    for(u in edge){
+		printf "%d:",id[u];
+		for(v in edge[u])printf " %d",id[v];
+		    print ""
+		}
+	    }' "$1" |
+	sort -n; # NAUTY expects the lines to be sorted by integer node ID, smallest first
+    echo ".${NL}x o" # this line ends the graph definition and then "x o" instructs dreadnaut to enumerate orbits
+) |
+    $DREADNAUT |
+    sed '1,/cpu time = .* seconds/d' | # the orbits are listed immediately following the "cpu time" line
+    sed 's/([0-9][0-9]*)//g' | # get rid of the number of nodes in parentheses
+    awk '# NAUTY annoyingly uses i1:i2 to specify nodes i1 through i2 inclusive; expand if necessary
+	function Expand(s,    t,n,a,j) {
+	    if(index(s,":")){
+		n=split(s,a,":")
+		if(n!=2){print "ERROR: nauty should have only 2 ints with a colon" > "/dev/fd/2"; exit 1}
+		t=a[1]
+		for(j=a[1]+1;j<=a[2];j++)t=t" "j
+	    }
+	    else t=s
+	    return t
+	}
+	{for(i=1;i<=NF;i++) printf "%s ",Expand($i)}' |
+    tr ';' "$NL" |
+    awk "ARGIND==1{$NAME2ID}"'
+	 ARGIND==2{ # read from the pipe on stdin
+	    # The following ensures there are no leading or trailing spaces
+	    printf "%s", name[$1]; for(i=2;i<=NF;i++) printf " %s", name[$i]
+	    print ""
+    }' "$1" - # the single hyphen specifies that the second "file" is awk's standard input (from the pipe)
+
