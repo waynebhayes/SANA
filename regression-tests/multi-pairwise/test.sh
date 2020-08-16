6fc79b45a5790de0ff78fcad1466e2b580796edb
waynebhayes <whayes@uci.edu>
2020-08-16 11:25:36 -0700

    reduced number regression meta-iters to 256

--- regression-tests/multi-pairwise/test.sh
+++ regression-tests/multi-pairwise/test.sh
@@ -13,7 +13,7 @@ MINSUM=0.25
 MEASURE="-ms3 1 -ms3_type 0"
  trap "/bin/rm -rf $DIR" 0 1 2 3 15
 if [ `hostname` = Jenkins ]; then
-    ITERS=999; minutes=0.1
+    ITERS=256; minutes=0.1
 else
     ITERS=99; minutes=0.1
 fi
