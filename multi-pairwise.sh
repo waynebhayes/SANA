ecc5b4307fc00447d8b5a41aec449fcd3fc1c78c
waynebhayes@github.com <whayes@uci.edu>
2020-10-22 22:08:50 -0700

    changed parallel-spec to just an int (0=all nets), and OUTDIR structure to dir(hundreds)/%100

--- multi-pairwise.sh
+++ multi-pairwise.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 USAGE="USAGE: $0 [-H N] {multi-SANA.exe} 'measures' iterations time-per-iter parallel-spec outdir {list of input networks}
-parallel-spec is one of the following:
-	a job name for distrib_sge (which only exists on UCI's openlab computers)
-	'-parallel K' (including the quotes!) for K processes locally
-	-1 for no parallelism (1 process at a time, very slow)
-
-multi-SANA.exe is the full path (including leading './' if necessary) to the multi-pairwise sana executable,
-    usually called 'sana.multi'"
+where:
+    multi-SANA.exe: full path (with leading './' if necessary) to sana.multi executable (usually './sana.multi')
+    parallel-spec is a non-negative integer saying how many parallel multi-SANA processes to run simultaneously, 0 for 'any'"
 
 NL='
 ' # newline
@@ -93,8 +89,6 @@ lss() {
     esac
 }
 
-export PARALLEL='distrib_sge "$JOB-$ITER" "$OUTDIR/dir$ITER"'
-
 TMPDIR=/tmp/multi-pairwise.$$
 trap "/bin/rm -rf $TMPDIR; exit" 0 1 2 3 15
 mkdir -p $TMPDIR
@@ -117,7 +111,7 @@ SANA_MULTI="$1"
 MEASURES="$2"
 ITER_EXPR="$3"
 T_ITER="$4"
-JOB="$5"
+PARALLEL="$5"
 OUTDIR="$6"
 NAME=`basename "$OUTDIR"`
 export SANA_MULTI ITER_EXPR NAME
@@ -125,17 +119,26 @@ shift 6
 
 [ -x "$SANA_MULTI" ] || die "first argument '$SANA_MULTI' must be an executable file"
 
-case "$JOB" in
-    -parallel*) PARALLEL='./parallel -s bash '`echo $JOB | awk '{print $NF}'`;;
-    -1) PARALLEL=bash ;; # no parallelism
-    *) ;; #[ -f "$JOB" ] || die "4th argument '$JOB' must be '-parallel N' or a job name for distrib_sge";;
+# Ensure "$PARALLEL" is an integer:
+[ `expr "$PARALLEL" '*' 1` -eq "$PARALLEL" ] || die "parallel-spec '$PARALLEL' must be a non-negative integer"
+# Ensure it's >= 0
+[ $PARALLEL -ge 0 ] || die "parallel-spec '$PARALLEL' must be a non-negative integer"
+
+case "$PARALLEL" in
+    0) PARALLEL="parallel -s bash $#";; # the number of networks
+    *) PARALLEL="parallel -s bash $PARALLEL";; # number of cores user specifies
 esac
 NUM_ITERS=`parse "$ITER_EXPR"` || die "'$ITER_EXPR': cannot figure out iteration count"
-if [ -d "$OUTDIR/dir-init" ]; then
+if [ -d "$OUTDIR/.init" ]; then
     warn "outdir '$OUTDIR' already exists; continuing"
 fi
+DIR_DIGITS=`echo -n "$NUM_ITERS" | wc -c` # num digits for "hundreds" directory names
+DirDigits() { # DirDigits is expect an integer on its STANDARD INPUT, not command line!
+    hawk '{ASSERT(ARGC==1 && NR==1 && NF==1 && $1*1==$1 && $1>=0);
+	printf "%0'$DIR_DIGITS'ds/%02d\n", 100*int($1/100), $1 % 100}' "$@"
+}
 
-mkdir -p $OUTDIR/dir-init || die "Cannot make outdir '$OUTDIR'"
+mkdir -p $OUTDIR/.init || die "Cannot make outdir '$OUTDIR'"
 
 # NOTE: REMAINDER OF THE COMMAND LINE IS ALL THE INPUT NETWORKS
 # We want them to be in alphabetical order
@@ -199,15 +202,16 @@ cat <<-EOF
 }
 
 # Create initial random alignment, which also tells us the number of nodes.
-[ -f "$OUTDIR"/dir-init/group.multiAlign ] || ./random-multi-alignment.sh $TYPES "$OUTDIR"/dir-init "$@"
+[ -f "$OUTDIR"/.init/group.multiAlign ] || ./random-multi-alignment.sh $TYPES "$OUTDIR"/.init "$@"
 
-mkdir -p $OUTDIR/dir0
+ZERO_DIR=`echo 0 | DirDigits`
+mkdir -p $OUTDIR/$ZERO_DIR
 
-SHADOW_GRAPH="$OUTDIR/dir0/$NAME-shadow0.gw"
-ShadowConfig $SHADOW_GRAPH $# "$@" 1 $OUTDIR/dir-init/*-shadow.align 0 > $OUTDIR/dir0/fshadow.txt
+SHADOW_GRAPH="$OUTDIR/$ZERO_DIR/$NAME-shadow0.gw"
+ShadowConfig $SHADOW_GRAPH $# "$@" 1 $OUTDIR/.init/*-shadow.align 0 > $OUTDIR/$ZERO_DIR/fshadow.txt
 # "-it 1" means perform 1 token iteration, since 0 is no longer allowed
-$SANA_MULTI -it 1 -mode shadow -fshadow $OUTDIR/dir0/fshadow.txt > $OUTDIR/dir0/fshadow.stdout 2>$OUTDIR/dir0/fshadow.stderr
-mv $OUTDIR/dir-init/*-shadow.align $OUTDIR/dir0
+$SANA_MULTI -it 1 -mode shadow -fshadow $OUTDIR/$ZERO_DIR/fshadow.txt > $OUTDIR/$ZERO_DIR/fshadow.stdout 2>$OUTDIR/$ZERO_DIR/fshadow.stderr
+mv $OUTDIR/.init/*-shadow.align $OUTDIR/$ZERO_DIR
 
 echo -n "Computing SES denominator..."
 export SES_DENOM=`numEdges "$@" | sort -n | awk '{m[NR-1]=$1}END{for(i=0;i<NR;i++) if(NR-i>=1){D+=(NR-i)^2*m[i];for(j=i+1;j<NR;j++)m[j]-=m[i]}; print D}'`
@@ -216,34 +220,35 @@ export EDGE_SUM=`echo "$ALL_EDGES" | add`
 export MAX_EDGE=`echo "$ALL_EDGES" | tail -1`
 echo Denominator for SES score is $SES_DENOM, EDGE_SUM is $EDGE_SUM, MAX_EDGE is $MAX_EDGE
 # Now get temperature schedule and SES denominator (the latter requires *.out files so don't use -scheduleOnly)
-mkdir -p $OUTDIR/dir-init
+mkdir -p $OUTDIR/.init
 /bin/rm -rf networks/$NAME-shadow0
-touch $OUTDIR/dir-init/schedule.tsv $OUTDIR/dir-init/tdecay.txt
+touch $OUTDIR/.init/schedule.tsv $OUTDIR/.init/tdecay.txt
 TIME_LIMIT=`parse "60*($T_ITER+20)"`
 TIME_LIMIT2=`parse "20*($TIME_LIMIT)"`
 if true; then
-    ITER=000
     TRIES=0
-    while [ `awk '{printf "%s.stdout\n", $1}' $OUTDIR/dir-init/schedule.tsv | tee $OUTDIR/dir-init/schedule.done | wc -l` -lt `echo name "$@" | wc -w` ]; do
+    while [ `awk '{printf "%s.stdout\n", $1}' $OUTDIR/.init/schedule.tsv | tee $OUTDIR/.init/schedule.done | wc -l` -lt `echo name "$@" | wc -w` ]; do
 	[ "$TRIES" -ge $MAX_TRIES ] && die "tried $TRIES times and failed to compute temperature schedule"
 	TRIES=`expr $TRIES + 1`
 	/bin/rm -rf networks/$NAME-shadow0
 	mkdir    -p networks/$NAME-shadow0; (cd networks/$NAME-shadow0; ln -s $TMPDIR/autogenerated .)
-	SHADOW_GRAPH="$OUTDIR/dir0/$NAME-shadow0.gw"
-	ls "$@" | awk '{file=$0;gsub(".*/",""); gsub(".el$",""); gsub(".gw$",""); printf "mkdir -p '$OUTDIR/dir-init';'"/usr/bin/time $TIME_VERBOSE $SANA_MULTI $MEASURES $TYPEargs"' -multi-iteration-only -it 1 -fg1 %s -fg2 '$SHADOW_GRAPH' -startalignment '$OUTDIR'/dir0/%s-shadow.align -o '$OUTDIR'/dir-init/%s >'$OUTDIR'/dir-init/%s.stdout 2>'$OUTDIR'/dir-init/%s.stderr\n", file,$0,$0,$0,$0}' | fgrep -v -f $OUTDIR/dir-init/schedule.done | tee $OUTDIR/dir-init/jobs.txt | eval $PARALLEL
-	awk '/^Computed TInitial .* in .*s$/{Tinit[FILENAME]=$3}/^Computed TFinal .* in .*s$/{Tfinal[FILENAME]=$3}/^TDecay needed to traverse this range:/{Tdecay[FILENAME]=$NF}END{for(i in Tinit)print i,Tinit[i],Tfinal[i],Tdecay[i]}' $OUTDIR/dir-init/*.stdout | sed -e "s,$OUTDIR/dir-init/,," -e 's/\.stdout//' > $OUTDIR/dir-init/tinitial-final.txt
-	echo 'name	tinitial	tfinal	tdecay' | tee $OUTDIR/dir-init/schedule.tsv
-	sed 's/ /	/g' $OUTDIR/dir-init/tinitial-final.txt | tee -a $OUTDIR/dir-init/schedule.tsv
+	SHADOW_GRAPH="$OUTDIR/$ZERO_DIR/$NAME-shadow0.gw"
+	ls "$@" | awk '{file=$0;gsub(".*/",""); gsub(".el$",""); gsub(".gw$",""); printf "mkdir -p '$OUTDIR/.init';'"/usr/bin/time $TIME_VERBOSE $SANA_MULTI $MEASURES $TYPEargs"' -multi-iteration-only -it 1 -fg1 %s -fg2 '$SHADOW_GRAPH' -startalignment '$OUTDIR/$ZERO_DIR'/%s-shadow.align -o '$OUTDIR'/.init/%s >'$OUTDIR'/.init/%s.stdout 2>'$OUTDIR'/.init/%s.stderr\n", file,$0,$0,$0,$0}' | fgrep -v -f $OUTDIR/.init/schedule.done | tee $OUTDIR/.init/jobs.txt | eval $PARALLEL
+	awk '/^Computed TInitial .* in .*s$/{Tinit[FILENAME]=$3}/^Computed TFinal .* in .*s$/{Tfinal[FILENAME]=$3}/^TDecay needed to traverse this range:/{Tdecay[FILENAME]=$NF}END{for(i in Tinit)print i,Tinit[i],Tfinal[i],Tdecay[i]}' $OUTDIR/.init/*.stdout | sed -e "s,$OUTDIR/.init/,," -e 's/\.stdout//' > $OUTDIR/.init/tinitial-final.txt
+	echo 'name	tinitial	tfinal	tdecay' | tee $OUTDIR/.init/schedule.tsv
+	sed 's/ /	/g' $OUTDIR/.init/tinitial-final.txt | tee -a $OUTDIR/.init/schedule.tsv
     done
 else
-    echo 'name	tinitial	tfinal	tdecay' | tee $OUTDIR/dir-init/schedule.tsv
-    ls "$@" | awk '{file=$0;gsub(".*/",""); gsub(".el$",""); gsub(".gw$","");printf "%s	40	1e-10	5\n",$1}' | tee -a $OUTDIR/dir-init/schedule.tsv
+    echo 'name	tinitial	tfinal	tdecay' | tee $OUTDIR/.init/schedule.tsv
+    ls "$@" | awk '{file=$0;gsub(".*/",""); gsub(".el$",""); gsub(".gw$","");printf "%s	40	1e-10	5\n",$1}' | tee -a $OUTDIR/.init/schedule.tsv
 fi
 case `hostname` in
 [Jj]enkins*) echo -n "Note: Jenkins will only show every 10 iterations";;
 esac
 for ITER in `integers $NUM_ITERS` `integers $NUM_ITERS $HILLCLIMB`
 do
+    ITER_dir=`echo $ITER | DirDigits`
+    i1_dir=`expr $ITER + 1 | DirDigits`
     /bin/rm -rf networks/*/autogenerated/*
     if [ "$VERBOSE" = "" ]; then
 	if [ `echo "$ITER % 10" | bc` -eq 0 ]; then echo ""; echo -n "ITER:"; fi
@@ -253,41 +258,41 @@ do
     fi
     if [ $ITER -ge $NUM_ITERS ]; then echo -n " (HillClimb)"; fi
     i1=`expr $ITER + 1`
-    if [ -f  $OUTDIR/dir$i1/jobs-done.txt ]; then
-       if [ `wc -l < $OUTDIR/dir$i1/jobs-done.txt` -eq `echo "$@" | wc -w` ]; then
+    if [ -f  $OUTDIR/$i1_dir/jobs-done.txt ]; then
+       if [ `wc -l < $OUTDIR/$i1_dir/jobs-done.txt` -eq `echo "$@" | wc -w` ]; then
 	    continue
 	fi
     fi
-    mkdir -p   $OUTDIR/dir$i1
-    /bin/rm -f $OUTDIR/dir$i1/expected-outFiles.txt
+    mkdir -p   $OUTDIR/$i1_dir
+    /bin/rm -f $OUTDIR/$i1_dir/expected-outFiles.txt
     for g
     do
 	bg=`basename $g .gw`
 	bg=`basename $bg .el`
-	echo $bg-shadow.align >> $OUTDIR/dir$i1/expected-outFiles.txt
-	SHADOW_GRAPH="$OUTDIR/dir$ITER/$NAME-shadow$ITER.gw"
-	# compute iterations to perform based on the iterations-per-second computed in dir-init for this network
-	SA_ITERS=`awk '/getPBad.*ips = /{ipsSum+=$NF}END{printf "%d\n", '$T_ITER'*60*ipsSum/NR}' $OUTDIR/dir-init/$bg.stdout`
-	awkcel '{if(name!="'$bg'")next; ITER='$ITER';NUM_ITERS='$NUM_ITERS'; e0=log(tinitial);e1=log(tfinal);printf "'"mkdir -p $OUTDIR/dir$i1; /bin/rm -f networks/$NAME-shadow$ITER/autogenerated/$NAME-shadow${ITER}_UntypedNodes_Unlocked_Weighted.bin; /usr/bin/time $TIME_VERBOSE $SANA_MULTI $MEASURES $TYPEargs -multi-iteration-only -it $SA_ITERS -fg1 $g -fg2 $SHADOW_GRAPH -tinitial %g -tdecay %g -o $OUTDIR/dir$i1/$bg-shadow >$OUTDIR/dir$i1/$bg-shadow.stdout 2>$OUTDIR/dir$i1/$bg-shadow.stderr -startalignment $OUTDIR/dir$ITER/$bg-shadow.align"'", tinitial*exp((e1-e0)*ITER/NUM_ITERS),tdecay/NUM_ITERS;print ""}' $OUTDIR/dir-init/schedule.tsv
-    done | sort -u > $OUTDIR/dir$ITER/jobs.txt
+	echo $bg-shadow.align >> $OUTDIR/$i1_dir/expected-outFiles.txt
+	SHADOW_GRAPH="$OUTDIR/$ITER_dir/$NAME-shadow$ITER.gw"
+	# compute iterations to perform based on the iterations-per-second computed in init for this network
+	SA_ITERS=`awk '/getPBad.*ips = /{ipsSum+=$NF}END{printf "%d\n", '$T_ITER'*60*ipsSum/NR}' $OUTDIR/.init/$bg.stdout`
+	awkcel '{if(name!="'$bg'")next; ITER='$ITER';NUM_ITERS='$NUM_ITERS'; e0=log(tinitial);e1=log(tfinal);printf "'"mkdir -p $OUTDIR/$i1_dir; /bin/rm -f networks/$NAME-shadow$ITER/autogenerated/$NAME-shadow${ITER}_UntypedNodes_Unlocked_Weighted.bin; /usr/bin/time $TIME_VERBOSE $SANA_MULTI $MEASURES $TYPEargs -multi-iteration-only -it $SA_ITERS -fg1 $g -fg2 $SHADOW_GRAPH -tinitial %g -tdecay %g -o $OUTDIR/$i1_dir/$bg-shadow >$OUTDIR/$i1_dir/$bg-shadow.stdout 2>$OUTDIR/$i1_dir/$bg-shadow.stderr -startalignment $OUTDIR/$ITER_dir/$bg-shadow.align"'", tinitial*exp((e1-e0)*ITER/NUM_ITERS),tdecay/NUM_ITERS;print ""}' $OUTDIR/.init/schedule.tsv
+    done | sort -u > $OUTDIR/$ITER_dir/jobs.txt
     /bin/rm -rf networks/$NAME-shadow$ITER
     TRIES=0
-    while [ `ls $OUTDIR/dir$i1 | fgrep -f $OUTDIR/dir$i1/expected-outFiles.txt | tee $OUTDIR/dir$ITER/jobs-done.txt | wc -l` -lt `echo "$@" | wc -w` ]; do
+    while [ `ls $OUTDIR/$i1_dir | fgrep -f $OUTDIR/$i1_dir/expected-outFiles.txt | tee $OUTDIR/$ITER_dir/jobs-done.txt | wc -l` -lt `echo "$@" | wc -w` ]; do
 	[ "$TRIES" -ge $MAX_TRIES ] && die "tried $TRIES times and failed to advance ITER from $ITER"
 	TRIES=`expr $TRIES + 1`
         /bin/rm -rf networks/*/autogenerated/*
-        sed -e 's/\.out//' -e 's/\.align/.stdout/' $OUTDIR/dir$ITER/jobs-done.txt | fgrep -v -f /dev/stdin $OUTDIR/dir$ITER/jobs.txt | eval $PARALLEL
+        sed -e 's/\.out//' -e 's/\.align/.stdout/' $OUTDIR/$ITER_dir/jobs-done.txt | fgrep -v -f /dev/stdin $OUTDIR/$ITER_dir/jobs.txt | eval $PARALLEL
     done
-    ./shadow2align.sh $OUTDIR/dir$i1/*.align > $OUTDIR/dir$i1/multiAlign.tsv
-    SHADOW_GRAPHi1="$OUTDIR/dir$i1/$NAME-shadow$i1.gw"
-    ShadowConfig "$SHADOW_GRAPHi1" $# "$@" 1 $OUTDIR/dir$i1/*-shadow.align 0 > $OUTDIR/dir$i1/fshadow.txt
-    $SANA_MULTI -it 1 -mode shadow -fshadow $OUTDIR/dir$i1/fshadow.txt > $OUTDIR/dir$i1/fshadow.stdout 2> $OUTDIR/dir$i1/fshadow.stderr || die "$OUTDIR/dir$i1/$NAME-shadow$i1.gw network creation failed"
-#   awk '{gsub("[|{}]","")}$'$SEScol'>1{sum2+=$'$SEScol'^2}END{printf " SES %g\n", sum2/'$SES_DENOM'}' $OUTDIR/dir$i1/$NAME-shadow$i1.gw
+    ./shadow2align.sh $OUTDIR/$i1_dir/*.align > $OUTDIR/$i1_dir/multiAlign.tsv
+    SHADOW_GRAPHi1="$OUTDIR/$i1_dir/$NAME-shadow$i1.gw"
+    ShadowConfig "$SHADOW_GRAPHi1" $# "$@" 1 $OUTDIR/$i1_dir/*-shadow.align 0 > $OUTDIR/$i1_dir/fshadow.txt
+    $SANA_MULTI -it 1 -mode shadow -fshadow $OUTDIR/$i1_dir/fshadow.txt > $OUTDIR/$i1_dir/fshadow.stdout 2> $OUTDIR/$i1_dir/fshadow.stderr || die "$OUTDIR/$i1_dir/$NAME-shadow$i1.gw network creation failed"
+#   awk '{gsub("[|{}]","")}$'$SEScol'>1{sum2+=$'$SEScol'^2}END{printf " SES %g\n", sum2/'$SES_DENOM'}' $OUTDIR/$i1_dir/$NAME-shadow$i1.gw
 done
 [ "$VERBOSE" = "" ] && echo "" # final newline
 echo "Now archiving .gw files using 7-zip to save space; press ^C to abort archiving"
 (cd "$OUTDIR" && 7z a shadow-gws.7z */*.gw && /bin/rm -f */*.gw)
 
 #echo "Computing CIQ... may take awhile..."
-#/CIQ.sh $OUTDIR/dir$i1/multiAlign.tsv `echo "$@" | newlines | sed 's/\.gw/.el/'`
+#/CIQ.sh $OUTDIR/$i1_dir/multiAlign.tsv `echo "$@" | newlines | sed 's/\.gw/.el/'`
 
