57538976437c55a0a17ca349bb146202151650da
waynebhayes <whayes@uci.edu>
2020-12-03 02:14:50 -0800

    update NetGO regression to get gene2go from go rather than resnik

--- regression-tests/NetGO/test.sh
+++ regression-tests/NetGO/test.sh
@@ -3,7 +3,7 @@ DIR=/tmp/NetGO.$$
 /bin/rm -rf $DIR
 mkdir -p $DIR
 trap "/bin/rm -rf $DIR" 0 1 2 3 15
-cp -p resnik/2018/*.gz $DIR
+cp -p go/2018/09/*.gz $DIR
 gunzip $DIR/*.gz
 unxz < regression-tests/multi-pairwise/BioGRID-100x1-dir300-multiAlign.tsv.xz > $DIR/BG.multi.tsv
 ./NetGO/NetGO.awk $DIR/go.obo $DIR/gene2go $DIR/BG.multi.tsv
