00e351ae17302b2c86cc4c06ae7ffd0a6d35d3e5
waynebhayes <whayes@uci.edu>
2021-07-31 06:17:26 -0700

    adding Nina's MMHS align file

--- regression-tests/NetGO/test-GOeval.sh
+++ regression-tests/NetGO/test-GOeval.sh
@@ -5,4 +5,6 @@ mkdir -p $DIR
 trap "/bin/rm -rf $DIR; exit" 0 1 2 3 15
 cp -p go/Entrez/2018/09/*.gz $DIR && gunzip $DIR/*.gz
 cp -p regression-tests/NetGO/*.xz $DIR && unxz $DIR/*.xz
+rm -f regression-tests/NetGO/MMHS1ec_60.align
+gunzip < regression-tests/NetGO/MMHS1ec_60.align.gz > regression-tests/NetGO/MMHS1ec_60.align
 GOeval.sh MM HS $DIR/MMusculus-3.4.164.el $DIR/HSapiens-3.4.164.el $DIR/go.obo $DIR/gene2go regression-tests/NetGO/MMHS1ec_60.align | fgrep -v '***' | diff -b - regression-tests/NetGO/GOeval.MMHS.correct
