cfd355e69bf92c1e2779c5c010c1dc5f23074a32
waynebhayes <whayes@uci.edu>
2021-08-03 02:43:09 -0700

    added EBM to GOeval.sh, along  with regression-test

--- regression-tests/NetGO/test-GOeval.sh
+++ regression-tests/NetGO/test-GOeval.sh
@@ -7,4 +7,9 @@ cp -p go/Entrez/2018/09/*.gz $DIR && gunzip $DIR/*.gz
 cp -p regression-tests/NetGO/*.xz $DIR && unxz $DIR/*.xz
 rm -f regression-tests/NetGO/MMHS1ec_60.align
 gunzip < regression-tests/NetGO/MMHS1ec_60.align.gz > regression-tests/NetGO/MMHS1ec_60.align
-GOeval.sh -pa MM HS $DIR/MMusculus-3.4.164.el $DIR/HSapiens-3.4.164.el $DIR/go.obo $DIR/gene2go regression-tests/NetGO/MMHS1ec_60.align | fgrep -v '***' | diff -b - <(unxz < regression-tests/NetGO/GOeval.MMHS.correct.xz)
+if GOeval.sh -ebm -pa MM HS $DIR/MMusculus-3.4.164.el $DIR/HSapiens-3.4.164.el $DIR/go.obo $DIR/gene2go regression-tests/NetGO/MMHS1ec_60.align | fgrep -v '***' | tee $DIR/GOeval.out | diff -b - <(unxz < regression-tests/NetGO/GOeval.MMHS.correct.xz); then
+    : # do nothing, success
+else
+    echo "failed; see $DIR for output" >&2
+    trap "" 0 1 2 3 15
+fi
