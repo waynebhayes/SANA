ec2d6d750dcaea770aa0e2e07ea347df9dfbf529
waynebhayes <whayes@uci.edu>
2022-03-11 20:28:58 -0800

    MAJOR overhaul of "parallel" to handle "auto" option

--- src/parallel.c
+++ src/parallel.c
@@ -1,12 +1,14 @@
-char USAGE[] = "\n\
-USAGE: parallel [-s [shell-name]] {n} \n\
+char USAGE[] = "\
+USAGE: parallel [-s [shell-name]] {n|auto} \n\
 parallel(1) reads command lines from the standard input, and keeps\n\
 {n} of them running concurrently.  It defaults to using the SHELL\n\
 environment variable as the executor; '-s' by itself will use the\n\
 Bourne Shell, and -s shell-name will use whatever shell you prefer\n\
 like rc(1).  To distinguish '-s {n}' from '-s {shell-name}', it assumes\n\
 your shell name doesn't start with a digit.\n\
-It exits with the number of failed jobs";
+It exits with the number of failed jobs.\n\
+With 'auto', it will attempt to keep all CPUs on the machine busy,\n\
+while attempting to account for jobs not under its control.";
 
 #include <strings.h>
 #include <string.h>
@@ -16,7 +18,8 @@ It exits with the number of failed jobs";
 #include <signal.h>
 #include <unistd.h>
 
-int parallel, numFailed;
+#define DELAY 60
+static int parallel, numFailed, cpus, load_av, AUTO;
 
 void SigHandler_changeParallel(int sig)
 {
@@ -41,15 +44,62 @@ void Fatal(char *s)
     exit(1);
 }
 
+int GetLoadAv(double minutes) {
+	// The three load averages are over 1, 5, and 15 minutes, respectively.
+	// Linux uload[1];ses "load average:", Darwin uses "load averages:" (with an 's'), thus the "s*" regexp.
+	FILE *fp = popen("uptime | tee /dev/tty | sed 's/.*load averages*: //' -e 's/,//g'", "r");
+	int i; float load[3];
+	for(i=0;i<3;i++) fscanf(fp, "%g", &load[i]);
+	// Take a weigthed average based on minutes since last check
+	if(minutes>15) load_av = load[2];
+	else if(minutes>5) load_av = (minutes*load[2] + (15-minutes)*load[1])/15;
+	else if(minutes>1) load_av = (minutes*load[1] + (5-minutes)*load[0])/5;
+	else load_av = load[0];
+	//fprintf(stderr, "load_av is %d ", load_av);
+	pclose(fp);
+	return ++load_av;
+}
+
+int SetParallel(double minutes, char *s)
+{
+    if(minutes < 1) return parallel; // do nothing, too short an interval
+    int ourLoad = parallel;
+    int othersLoad = (GetLoadAv(minutes) - ourLoad);
+    if(othersLoad<0) othersLoad=0;
+    parallel = cpus - othersLoad;
+    fprintf(stderr, "%s minutes %.1f ourLoad %d load_av %d others %d; parallel set to %d",
+	s, minutes, ourLoad, load_av, othersLoad, parallel);
+    if(parallel<1) {parallel=1; fprintf(stderr, " (corrected to %d)\n", parallel);}
+    else if(parallel>cpus) {parallel=cpus; fprintf(stderr, " (corrected to %d)\n", parallel);}
+    else fprintf(stderr,"\n");
+    return parallel;
+}
+
+void SigAlarmHandler(int sig)
+{
+    static int numDelaysSinceLastCheck;
+    ++numDelaysSinceLastCheck;
+    fprintf(stderr,"."); fflush(stderr);
+    double minutesSinceLastCheck = DELAY*numDelaysSinceLastCheck/60.0;
+    if(minutesSinceLastCheck>=1) {
+	SetParallel(minutesSinceLastCheck, "alarm;");
+	numDelaysSinceLastCheck = 0;
+    }
+    alarm(DELAY);
+}
+
 
 int main(int argc, char *argv[])
 {
-    int n=0, n_arg = 1, status;
+    int numRunning=0, n_arg = 1, status;
     char line[1024000];
     char *SHELL = getenv("SHELL"), *SH_argv0;
 
     switch(argc)
     {
+    case 1:
+	Fatal(USAGE);
+	break;
     case 2:	/* no args other than n */
 	n_arg = 1;
 	break;
@@ -73,21 +123,35 @@ int main(int argc, char *argv[])
 	break;
     }
 
-    signal(SIGUSR1, SigHandler_changeParallel);
-    signal(SIGUSR2, SigHandler_changeParallel);
-
     SH_argv0 = rindex(SHELL, '/');	/* basename of SHELL */
     if(SH_argv0 == NULL)
 	SH_argv0 = SHELL;
 
-    parallel = atoi(argv[n_arg]);
+    if(strcmp(argv[n_arg],"auto")==0){
+	AUTO=1;
+	FILE *fp = popen("cpus","r");
+	fscanf(fp, "%d", &cpus);
+	pclose(fp);
+	if(cpus<1) Fatal("could not determine number of CPUs");
+	parallel = cpus-GetLoadAv(0.0);
+	if(parallel<=0) parallel=1;
+	fprintf(stderr, "using AUTO with number of cpus %d; current load %d; initial parallel %d\n", cpus, load_av, parallel);
+	signal(SIGALRM, SigAlarmHandler);
+	alarm(DELAY);
+    }
+    else
+	parallel = atoi(argv[n_arg]);
+
+    signal(SIGUSR1, SigHandler_changeParallel);
+    signal(SIGUSR2, SigHandler_changeParallel);
+
     while(fgets(line, sizeof(line), stdin))
     {
-	while(n >= parallel)
+	while(numRunning >= parallel)
 	{
 	    wait(&status);
 	    if(!WIFEXITED(status) || WEXITSTATUS(status)) ++numFailed;
-	    --n;
+	    --numRunning;
 	}
 	if(fork() == 0)  /* child */
 	{
@@ -97,16 +161,16 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-	    if(++n >= parallel)
+	    if(++numRunning >= parallel)
 	    {
 		wait(&status);
 		if(!WIFEXITED(status) || WEXITSTATUS(status)) ++numFailed;
-		--n;
+		--numRunning;
 	    }
 	}
     }
 #if 1
-    while(n-- > 0) { /* wait for everyone to finish */
+    while(numRunning-- > 0) { /* wait for everyone to finish */
 	wait(&status);
 	if(!WIFEXITED(status) || WEXITSTATUS(status)) ++numFailed;
     }
