0d39c86ee1698bf8b33b44880a024cfb33aea1e8
Wayne B. Hayes <whayes@uci.edu>
2022-04-05 19:16:33 -0700

    adding s3 values into NAF-netbits-support by inducing and running sana-analyze.sh

--- scripts/NAF-netbits-support.sh
+++ scripts/NAF-netbits-support.sh
@@ -27,34 +27,59 @@ TMPDIR=`mktemp -d /tmp/$BASENAME.XXXXXX`
 net1=$1
 net2=$2
 shift 2
+count2_el=`count.el "$net2"`
+n2=`echo $count2_el | awk '{print $1}'`
+m2=`echo $count2_el | awk '{print $2}'`
 
-[ $# -eq 100 ] || die "expecting 100 alignment files"
-[ `ls -d "$@" | fgrep -c .align` -eq 100 ] || die "expecting 100 alignment files"
+[ $# -eq 100 ] || warn "expecting 100 alignment files but got $#"
+numFiles=`ls -d "$@" | fgrep -c .align`
+[ $numFiles -eq 100 ] || warn "expecting 100 alignment files but got $numFiles"
 
+DIR=`dirname "$1"`
+echo -n "Processing alignment files: " >&2
 for i; do
+    echo -n " $i" >&2
     case "$i" in
-    *.align) b=`basename $i .align`;;
+    *.align) b=`echo $i | sed 's/\.align$//'`;;
     *) die "'$i' is not an alignment file";;
     esac
-    ccs-el.sh "$net1" "$net2" $i | tee $TMPDIR/$b.ccs-el | awk '{printf "\t%s\t\n",$0}' > $TMPDIR/$b.ccs-el.tt
     [ -f $b.resnik.NOSEQ ] || die "expecting $b.resnik.NOSEQ to exist"
+    [ -f $b.ccs-el.tt ] || ccs-el.sh "$net1" "$net2" $i | tee $b.ccs-el | awk '{printf "\t%s\t\n",$0}' > $b.ccs-el.tt
 done
+echo "Done" >&2
 
-( echo 'NAF	u	v	Resnik'; sort [A-Z][A-Z][A-Z][A-Z][0-9]*_60.resnik.NOSEQ | uniq -c | sort -nr | sed 's/max	//' -e 's/^  *//' -e 's/ /	/g') > $TMPDIR/NAF-uv-resnik.NOSEQ.tsv
+echo -n "Creating NAF-uv-resnik... " >&2
+[ -f $DIR/NAF-uv-resnik.NOSEQ.tsv ] || ( echo 'NAF	u	v	Resnik'; sort [A-Z][A-Z][A-Z][A-Z][0-9]*_60.resnik.NOSEQ | uniq -c | sort -nr | sed 's/max	//' -e 's/^  *//' -e 's/ /	/g') > $DIR/NAF-uv-resnik.NOSEQ.tsv
+echo "Done" >&2
 
-NAFs=`awkcel '{print NAF}' $TMPDIR/NAF-uv-resnik.NOSEQ.tsv | uniq`
+NAFs=`awkcel '{print NAF}' $DIR/NAF-uv-resnik.NOSEQ.tsv | uniq`
 
+echo "Processing NAF values:" >&2
 for N in $NAFs; do
-    awk 'NR>1&&$1>='$N'{printf "%s,%s\n",$2,$3}' $TMPDIR/NAF-uv-resnik.NOSEQ.tsv | # get the list of aligned pairs above NAF threshold
-	tee $TMPDIR/NAF.$N.nodes |
-	awk '{printf "\t%s\t\n",$0}' | # add leading and trailing tabs to isolate them for fgrep...
-	fgrep -l -f - $TMPDIR/*.ccs-el.tt | sed 's/\.tt$//' | # get rid of the .tt extension to cat only the ccs-el files...
-	xargs cat > $TMPDIR/NAF.$N.el # use "cat", not "sort -u", to count edges, including dups, that support a NAF-aligned pair
-    count_el=`induce $TMPDIR/NAF.$N.nodes $TMPDIR/NAF.$N.el | # get the NAF-induced subgraph on the multi-edged network
-	count.el -`
-    echo $N $count_el |
-	hawk '{NAF=$1; n=$2;m=$3;b=netbits(n); if(m) print NAF,n,m,b,m/b}' # print NAF, nodes, edges, netbits, edges/netbits
-done | revlines | # reverse the lines so we see the LOWEST NAF values first...
-    awk 'BEGIN{print "NAF	nodes	edges	netbits	ratio	avgRatio"}
-	{sum+=$NF; print $0,sum/NR}' | # and then print the above lines with the AVERAGE support up to this NAF
-	sed 's/^  *//' -e 's/ *$//' -e 's/  */	/g' # make it a TSV
+    #echo -n "Getting s3 values of NAF=$N for" >&2
+    [ -f $DIR/NAF.$N.nodes ] ||
+	awk 'NR>1&&$1>='$N'{printf "%s,%s\n",$2,$3}' $DIR/NAF-uv-resnik.NOSEQ.tsv | # list of aligned pairs above NAF threshold
+	    tee $DIR/NAF.$N.nodes > /dev/null # |
+#	    awk '{printf "\t%s\t\n",$0}' | # add leading and trailing tabs to isolate them for fgrep...
+#	    fgrep -h -f - $DIR/*.ccs-el.tt | sed 's/^\t//' -e 's/\t$//' |
+#	    sort -u > $DIR/NAF.$N.el # | cat instead of "sort -u" to include dups that support a NAF-aligned pair?
+    for i; do
+	cut -d, -f1 $DIR/NAF.$N.nodes | tee $TMPDIR/NAF.$N.V1 | awk 'ARGIND==1{induce[$1]=1}ARGIND==2 && $1 in induce{print}' - $i > $TMPDIR/NAF.$N.$i
+	cut -d, -f2 $DIR/NAF.$N.nodes > $TMPDIR/NAF.$N.V2
+	induce $TMPDIR/NAF.$N.V1 "$net1" >> $TMPDIR/NAF.$N.E1
+	induce $TMPDIR/NAF.$N.V2 "$net2" >> $TMPDIR/NAF.$N.E2
+	echo "sana-analyze.sh s3 $net1 $net2 $TMPDIR/NAF.$N.$i > $TMPDIR/NAF.$N.$i.s3"
+    done | parallel `cores`
+    #echo " Done S3s for NAF $N" >&2
+    m1=`wc -l < $TMPDIR/NAF.$N.E1 | awk '{print $1/'$numFiles'}'`
+    m2=`wc -l < $TMPDIR/NAF.$N.E2 | awk '{print $1/'$numFiles'}'`
+    s3=`cat $TMPDIR/NAF.$N.*.s3 | avg`
+    m=`parse.awk "int(($m1+$m2)*$s3)"`  # this is a horrible approximation to the number of purple edges!
+    #echo "m1 $m1 m2 $m2 s3 $s3 m $m n2 $n2" >&2
+    echo $N `wc -l < $DIR/NAF.$N.nodes` $m |
+	hawk '{NAF=$1; n=$2; m=$3; if(m){b=netbits(n,'"$n2"'); print NAF,n,m,b,'"$s3"'}}'
+done | revlines | tee $TMPDIR/final-pipe.txt | # reverse the lines so we see the LOWEST NAF values first to initialize avRatio small
+    hawk 'NR==1{print "NAF	nodes	edges	netBits	ratio	s3	avRatio	avS3"}
+	{NAF=$1; n=$2; m=$3; b=$4; s3=$5; sumF+=m/b; sumS3+=s3;
+	 printf "%3d\t%5d\t%s\t%s\t%s\t%.5f\t%s\t%s\n", NAF, n, ftos(m), ftos(b), ftos(m/b), s3, ftos(sumF/NR), ftos(sumS3/NR)
+	}'
