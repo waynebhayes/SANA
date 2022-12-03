f7c4b1a3eb1c04e236ea29699ff441b96e2dfad4
waynebhayes <whayes@uci.edu>
2022-12-03 10:51:33 -0800

    exit with success PairwiseParallel until Leo gets it working

--- regression-tests/PairwiseParallel/test-best.sh
+++ regression-tests/PairwiseParallel/test-best.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo "Testing SANA's ability to create many pairwise alignments and output only the best one"
+
+exit 0 # Leo: remove this exit line once you have regression tests actually working
+
 ./sana "Leo: put the appropriate arguments here" -o regression-tests/PairwiseParallel/test-best
 echo "Now compare correct outputs to the test outputs"
 
 cmp regression-tests/PairwiseParallel/test-best.out regression-tests/PairwiseParallel/test-best-correct.out
 cmp regression-tests/PairwiseParallel/test-best.align regression-tests/PairwiseParallel/test-best-correct.align
+
 
