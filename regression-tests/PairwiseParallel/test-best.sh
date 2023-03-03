432d64b67df6c5c6af1f25457f88f537c3d1e28a
waynebhayes@github.com <whayes@uci.edu>
2023-03-03 14:49:27 -0800

    updates for Leo

--- regression-tests/PairwiseParallel/test-best.sh
+++ regression-tests/PairwiseParallel/test-best.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Testing SANA's ability to create many pairwise alignments and output only the best one"
 
-exit 0 # Leo: remove this exit line once you have regression tests actually working
+#exit 0 # Leo: remove this exit line once you have regression tests actually working
 
-./sana "Leo: put the appropriate arguments here" -o regression-tests/PairwiseParallel/test-best
+./sana -fg1 networks/RNorvegicus.el -fg2 networks/SPombe.el -ec 1 -parallelTotalAlign 4 -parallelBatch 2 -parallelReportAll 1 -o regression-tests/PairwiseParallel/test-best
+
 echo "Now compare correct outputs to the test outputs"
 
 cmp regression-tests/PairwiseParallel/test-best.out regression-tests/PairwiseParallel/test-best-correct.out
