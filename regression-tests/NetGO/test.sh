1bc58469c1dbe91c24133a4a710d1b82ff193954
waynebhayes@github.com <whayes@uci.edu>
2023-03-22 08:53:32 -0700

    fixed NetGO test output

--- regression-tests/NetGO/test.sh
+++ regression-tests/NetGO/test.sh
@@ -6,4 +6,12 @@ trap "/bin/rm -rf $DIR" 0 1 2 3 15
 cp -p resnik/2018/*.gz $DIR
 gunzip $DIR/*.gz
 unxz < regression-tests/multi-pairwise/BioGRID-100x1-dir300-multiAlign.tsv.xz > $DIR/BG.multi.tsv
-./NetGO/NetGO.awk $DIR/go.obo $DIR/gene2go $DIR/BG.multi.tsv
+./NetGO/NetGO.awk $DIR/go.obo $DIR/gene2go $DIR/BG.multi.tsv | egrep -v '^---' | sed 's,^/tmp/.*: ,,' > $DIR/test.out
+if unxz < regression-tests/NetGO/MMHS1ec_60-correct.out.xz | cmp - $DIR/test.out; then
+    echo passed
+else
+    echo "failed; see output in $DIR"
+    unxz < regression-tests/NetGO/MMHS1ec_60-correct.out.xz > $DIR/correct.out
+    trap "" 0 1 2 3 15
+    exit 1
+fi
