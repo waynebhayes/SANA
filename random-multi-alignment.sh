431cf6e2891341c04016704035c9b1b6661d52b5
waynebhayes@github.com <whayes@uci.edu>
2023-10-06 14:31:27 -0700

    removed TYPES from multiple alignments and made random-multi-alignment ACTUALLY random

--- random-multi-alignment.sh
+++ random-multi-alignment.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-USAGE="USAGE: $0 {true | false, representing --bipartite} OUTDIR {list of network files, LEDA or edgelist accepted}"
+USAGE="USAGE: $0 OUTDIR {list of network files, LEDA or edgelist accepted}"
 die() { echo "ERROR: $@" >&2; echo "$USAGE" >&2; exit 1
 }
 
@@ -19,12 +19,6 @@ cat "$@" | awk 'BEGIN{srand();srand(int(2^30*rand())+PROCINFO["pid"]+PROCINFO["p
     }'
 }
 
-case "$1" in
-    true|false) ;;
-    *) die "first argument must be 'true' or 'false', representing whether nodes have types" ;;
-esac
-TYPES="$1"; shift
-
 OUTDIR="$1"; shift
 [ -d "$OUTDIR" ] || die "first argument must be name of output directory, and it must already exist"
 
@@ -33,7 +27,7 @@ TMPDIR=/tmp/multi-pairwise-random$$
 mkdir $TMPDIR
 GROUP=''
 
-if $TYPES; then readCols=1; else readCols=2; fi # read the second column later if TYPES
+readCols=2 # read the second column 
 # NOTE: the above means that, when bipartite, nodes are internally numbered as follows:
 # first we read through the first column ONLY, incremeting numNodes as we encounter previously unseen nodes;
 # then we do the same for the second column.
@@ -45,18 +39,14 @@ do
     [ -f $TMPDIR/$b.1.nodes ] && die "argument $i: already have a network called $b"
     GROUP="$GROUP-$b"
     case "$i" in
-    *.gw) if $TYPES; then die "network '$i': can't accept .gw files with -bipartite";fi
-	awk '/^.{/&&NF==1{print}' $i | sed 's/[|{}]//g';;
+    *.gw) awk '/^.{/&&NF==1{print}' $i | sed 's/[|{}]//g';;
     *.el) awk 'BEGIN{numNodes=0}{for(i=1;i<='$readCols';i++)if(!seen[$i]){seen[$i]=1;name[numNodes++]=$i}}END{for(i=0;i<numNodes;i++)print name[i]}' $i;;
-    esac | tee $TMPDIR/$b.1.nodes | wc -l > $TMPDIR/$b.1.numNodes
-    if $TYPES; then awk '{i=2;if(!seen[$i]){seen[$i]=1;name[numNodes++]=$i}}END{for(i=0;i<numNodes;i++)print name[i]}' $i | tee $TMPDIR/$b.2.nodes | wc -l > $TMPDIR/$b.2.numNodes; fi
+    esac | randomizeLines | tee $TMPDIR/$b.1.nodes | wc -l > $TMPDIR/$b.1.numNodes
 done
 #GROUP=`echo " $GROUP" | sed 's/^ -//'`
 GROUP=group
 #SHADOW_NODES=`cd $TMPDIR && grep . *.numNodes | sed -e 's/\.numNodes//' -e 's/:/ /' | sort -k 2n | tail -1 | awk '{print $2}'`
-echo "WARNING: this isn't actually random yet, it's a fixed starting alignment" >&2
 paste $TMPDIR/*.1.nodes | sed -e 's/		/	_	/g' -e 's/		/	_	/g' -e 's/^	/_	/' -e 's/	$/	_/' > $TMPDIR/$GROUP.multiAlign
-paste $TMPDIR/*.2.nodes 2>/dev/null | sed -e 's/		/	_	/g' -e 's/		/	_	/g' -e 's/^	/_	/' -e 's/	$/	_/' >> $TMPDIR/$GROUP.multiAlign
 
 (
     cd $TMPDIR
@@ -65,11 +55,7 @@ paste $TMPDIR/*.2.nodes 2>/dev/null | sed -e 's/		/	_	/g' -e 's/		/	_	/g' -e 's/
     do
 	b=`basename $i .1.nodes`
 	N=`expr $N + 1`
-	cut -f$N $GROUP.multiAlign | if $TYPES; then
-	    awk '$1!="_"{if(/MNEST/)printf("%s\tmRNA-%04d\n",$1,MNEST++);else printf("%s\t%s\n",$1,$1);}'
-	else
-	    awk '$1!="_"{printf("%s\tshad_%d\n",$1,NR-1);}'
-	fi > $b-shadow.align
+	cut -f$N $GROUP.multiAlign | awk '$1!="_"{printf("%s\tshad_%d\n",$1,NR-1);}' > $b-shadow.align
     done
 )
 mv $TMPDIR/*[Aa]lign "$OUTDIR"
