6db931fac21029c43f413a8d4113c7dcf06677bd
malonog03 <mlonog2021@gmail.com>
2025-08-07 12:23:30 -0400

    Fixed alignment bug: locked holes were not being properly locked or unlocked in SANAThree::chooseNextRequest()

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -24,7 +24,7 @@ bool myNan(double x);
 
 double randDouble();
 int randInt(int low, int high);
-int randInt(int low, int high, mt19937 &gen);
+unsigned randUnsigned(unsigned low, unsigned high, mt19937 &gen);
 int randMod(int n);
 void randomShuffle(vector<uint>& v);
 
